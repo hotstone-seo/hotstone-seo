e0ddb8a66a677157ce431317ec79646eb2de5d99
hendri <hendri.chia@tiket.com>
2019-11-27 14:34:38 +0700

    removed unused code

--- src/_nav.js
+++ src/_nav.js
@@ -20,33 +20,6 @@ export default {
       },
     },
     {
-      title: true,
-      name: 'Theme',
-      wrapper: {            // optional wrapper object
-        element: '',        // required valid HTML5 element tag
-        attributes: {}        // optional valid JS object with JS API naming ex: { className: "my-class", style: { fontFamily: "Verdana" }, id: "my-id"}
-      },
-      class: ''             // optional class names space delimited list for title item ex: "text-center"
-    },
-    {
-      name: 'Colors',
-      url: '/theme/colors',
-      icon: 'icon-drop',
-    },
-    {
-      name: 'Typography',
-      url: '/theme/typography',
-      icon: 'icon-pencil',
-    },
-    {
-      title: true,
-      name: 'Components',
-      wrapper: {
-        element: '',
-        attributes: {},
-      },
-    },
-    {
       name: 'Base',
       url: '/base',
       icon: 'icon-puzzle',
@@ -246,21 +219,6 @@ export default {
         {
           name: 'Login',
           url: '/login',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Register',
-          url: '/register',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Error 404',
-          url: '/404',
-          icon: 'icon-star',
-        },
-        {
-          name: 'Error 500',
-          url: '/500',
           icon: 'icon-star',
         },
       ],
