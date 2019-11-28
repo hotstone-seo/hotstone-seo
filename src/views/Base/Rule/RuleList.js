d2858adba572c54dd7d14c6e35403b42eb78d70b
hendri <hendri.chia@tiket.com>
2019-11-28 16:21:02 +0700

    remove unused code

--- src/views/Base/Rule/RuleList.js
+++ src/views/Base/Rule/RuleList.js
@@ -94,18 +94,13 @@ class RuleList extends Component {
                   </PaginationItem>
                   <PaginationItem className="page-item"><PaginationLink tag="button">2</PaginationLink></PaginationItem>
                   <PaginationItem><PaginationLink tag="button">3</PaginationLink></PaginationItem>
-                  <PaginationItem><PaginationLink tag="button">4</PaginationLink></PaginationItem>
                   <PaginationItem><PaginationLink next tag="button">Next</PaginationLink></PaginationItem>
                 </Pagination>
               </CardBody>
             </Card>
-          </Col>
-
+         </Col>
         </Row>
-
-        
       </div>
-
     );
   }
 }
