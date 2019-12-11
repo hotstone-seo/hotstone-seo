4fceb39ad3a28a7c35e4996ec1e936b736ef7dfb
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-12-11 16:01:21 +0700

    rule.data_source_id is nullable; Remove rule.exclusion
    Co-Authored-By: hendrichiatiket <hendrichiatiket@users.noreply.github.com>

--- app/repository/rule_repo.go
+++ app/repository/rule_repo.go
@@ -9,8 +9,7 @@ type Rule struct {
 	ID           int64     `json:"id"`
 	Name         string    `json:"name" validate:"required"`
 	UrlPattern   string    `json:"url_pattern" validate:"required"`
-	Exclusion    *string   `json:"exclusion"`
-	IdDataSource int64     `validate:"required"`
+	DataSourceID *int64    `validate:"required"`
 	UpdatedAt    time.Time `json:"-"`
 	CreatedAt    time.Time `json:"-"`
 }
--- scripts/db/migration/1_rules.up.sql
+++ scripts/db/migration/1_rules.up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE rules (
     id serial PRIMARY KEY,
+    data_source_id INTEGER,
     "name" VARCHAR (255) NOT NULL,
     url_pattern TEXT NOT NULL,
-    exclusion TEXT,
-    id_data_source INTEGER NOT NULL,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
