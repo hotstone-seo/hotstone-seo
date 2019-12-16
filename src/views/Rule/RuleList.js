af955fcf9771d295c69700729624ec6e2cf141ca
hendri <hendri.chia@tiket.com>
2019-12-16 14:01:29 +0700

    read data from REST API

--- src/views/Rule/RuleList.js
+++ src/views/Rule/RuleList.js
@@ -10,12 +10,49 @@ class RuleList extends Component {
     this.state = {
       rules: [],
     };
+
+    this.columns = [
+      {
+        title: 'Rule ID',
+        dataIndex: 'id',
+        key: 'id',
+      },
+      {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+      },
+      {
+        title: 'URL Pattern',
+        dataIndex: 'url_pattern',
+        key: 'url_pattern',
+      },
+      {
+        title: 'Updated At',
+        dataIndex: 'updated_at',
+        key: 'updated_at',
+      },
+      {
+        title: 'Actions',
+        key: 'actions',
+        render: (text, record) => (
+          <span>
+            <Button type="link" onClick={() => this.handleClick(record)}>Edit</Button>
+            <Divider type="vertical" />
+            <Popconfirm title="Are you sure want to delete?" onConfirm={() => this.handleDelete(record.id)}>
+              <Button type="link">Delete</Button>
+            </Popconfirm>
+          </span>
+        ),
+      },
+    ];
     this.handleClick = this.handleClick.bind(this);
     this.handleEdit = this.handleEdit.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   componentDidMount() {
-    axios.get('http://localhost:4000/environments')
+    axios.get('http://localhost:4000/rules')
       .then((res) => {
         const rules = res.data;
         this.setState({ rules });
@@ -32,8 +69,20 @@ class RuleList extends Component {
     history.push('/ruleEditForm');
   }
 
+  handleDelete(id) {
+    axios.delete(`http://localhost:4000/rules/${id}`)
+      .then(() => {
+        const { rules } = this.state;
+        this.setState({ rules: rules.filter((env) => env.id !== id) });
+      })
+      .catch((error) => {
+        message.error(error.message);
+      });
+  }
+
   render() {
-    const ruleLink = '/ruleDetail/100'
+    const { rules } = this.state;
+   
     return (
       <div className="animated fadeIn">
         <Col xs="12" lg="12">
@@ -45,28 +94,12 @@ class RuleList extends Component {
               <div style={{ marginBottom: '.5rem' }}>
                 <Button color="primary" onClick={this.handleClick}>Add New</Button>
               </div>
-              <Table responsive bordered>
-                <thead>
-                  <tr>
-                    <th>Name</th>
-                    <th>URL Pattern</th>
-                    <th>Data Source</th>
-                    <th>Updated Date</th>
-                    <th>Action</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td><Link to={ruleLink}>Airport Detail</Link></td>
-                    <td>http://xxx</td>
-                    <td>DataSource1</td>
-                    <td>Nov 15 2019</td>
-                    <td>
-                      <NavLink href="#" onClick={this.handleEdit}>Edit</NavLink>
-                    </td>
-                  </tr>
-                </tbody>
-              </Table>
+              <Table responsive bordered
+                dataSource={rules}
+                columns={this.columns}
+                rowKey={(record) => record.id}
+              />
+              
               <Pagination>
                 <PaginationItem><PaginationLink previous tag="button">Prev</PaginationLink></PaginationItem>
                 <PaginationItem active>
