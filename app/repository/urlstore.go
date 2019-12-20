c284007cdcabbdbd7e49b4d5ae41fd7915922fc8
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-12-20 14:49:57 +0700

    URLStore implementation

---
+++ app/repository/urlstore.go
@@ -0,0 +1,79 @@
+package repository
+
+import (
+	"strconv"
+
+	"github.com/labstack/gommon/log"
+)
+
+type VarMap map[string]string
+
+type URLStore interface {
+	GetURL(path string) (int, VarMap)
+	AddURL(id int, key string)
+	UpdateURL(id int, key string)
+	DeleteURL(id int) bool
+	Count() int
+}
+
+// URLStoreImpl is implementation of RuleService
+type URLStoreImpl struct {
+	store URLStoreTree
+}
+
+// NewURLStore return new instance of RuleService
+func NewURLStore() URLStore {
+	return &URLStoreImpl{store: newURLStoreTree()}
+}
+
+func (s *URLStoreImpl) GetURL(path string) (int, VarMap) {
+	maxParams := 256
+
+	pvalues := make([]string, maxParams)
+
+	varValue := VarMap{}
+
+	data, pnames := s.store.Get(path, pvalues)
+	if data == nil {
+		return -1, varValue
+	}
+
+	if len(pnames) > 0 {
+		for i, name := range pnames {
+			varValue[name] = pvalues[i]
+		}
+	}
+
+	idStr, ok := data.(string)
+	if !ok {
+		log.Warnf("[GetURL] Failed to cast data to string. data=%+v", data)
+		return -1, varValue
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		log.Warnf("[GetURL] Failed to convert string data to int. idStr=%+v", idStr)
+		return -1, varValue
+	}
+
+	return id, varValue
+}
+
+func (s *URLStoreImpl) AddURL(id int, key string) {
+	data := strconv.Itoa(id)
+
+	s.store.Add(id, key, data)
+}
+
+func (s *URLStoreImpl) UpdateURL(id int, key string) {
+	s.DeleteURL(id)
+	s.AddURL(id, key)
+}
+
+func (s *URLStoreImpl) DeleteURL(id int) bool {
+	return s.store.Delete(id)
+}
+
+func (s *URLStoreImpl) Count() int {
+	return s.store.Count()
+}
---
+++ app/repository/urlstore_test.go
@@ -0,0 +1,145 @@
+package repository
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestURLStoreImpl_GetURL(t *testing.T) {
+
+	t.Run("WHEN static url not exist", func(t *testing.T) {
+		store := buildStore(t)
+
+		id, varMap := store.GetURL("/gopher/doc.jpg")
+		require.Equal(t, -1, id)
+		require.Empty(t, varMap)
+	})
+
+	t.Run("WHEN static url exist", func(t *testing.T) {
+		store := buildStore(t)
+
+		id, varMap := store.GetURL("/gopher/doc.png")
+		require.Equal(t, 6, id)
+		require.Empty(t, varMap)
+	})
+
+	t.Run("WHEN param url not exist", func(t *testing.T) {
+		store := buildStore(t)
+
+		id, varMap := store.GetURL("/users/def/abc")
+		require.Equal(t, -1, id)
+		require.Empty(t, varMap)
+	})
+
+	t.Run("WHEN param url exist", func(t *testing.T) {
+		store := buildStore(t)
+
+		id, varMap := store.GetURL("/users/def/123")
+		require.Equal(t, 12, id)
+
+		require.Equal(t, 2, len(varMap))
+		require.Equal(t, "def", varMap["id"])
+		require.Equal(t, "123", varMap["accnt"])
+	})
+}
+
+func TestURLStoreImpl_AddURL(t *testing.T) {
+
+	t.Run("WHEN new static url added AND id not exist before", func(t *testing.T) {
+		store := buildStore(t)
+
+		url := "/gopher/doc.jpg"
+		store.AddURL(20, url)
+
+		id, varMap := store.GetURL(url)
+		require.Equal(t, 20, id)
+		require.Empty(t, varMap)
+		require.Equal(t, 10, store.Count())
+	})
+
+	t.Run("WHEN new static url added AND id exist before THEN double data added (with same id)", func(t *testing.T) {
+		store := buildStore(t)
+
+		oldUrl := "/gopher/doc.jpg"
+		newUrl := "/gopher/doc.img"
+
+		store.AddURL(20, oldUrl)
+		store.AddURL(20, newUrl)
+
+		id, varMap := store.GetURL(newUrl)
+		require.Equal(t, 20, id)
+		require.Empty(t, varMap)
+
+		id, varMap = store.GetURL(oldUrl)
+		require.Equal(t, 20, id)
+		require.Empty(t, varMap)
+
+		require.Equal(t, 11, store.Count())
+	})
+}
+
+func TestURLStoreImpl_UpdateURL(t *testing.T) {
+	t.Run("WHEN existing static url updated with different url", func(t *testing.T) {
+		store := buildStore(t)
+
+		oldUrl := "/gopher/doc.png"
+		updatedUrl := "/gopher/doc.bmp"
+		store.UpdateURL(6, updatedUrl)
+
+		id, varMap := store.GetURL(oldUrl)
+		require.Equal(t, -1, id)
+		require.Empty(t, varMap)
+
+		id, varMap = store.GetURL(updatedUrl)
+		require.Equal(t, 6, id)
+		require.Equal(t, 0, len(varMap))
+
+		require.Equal(t, 9, store.Count())
+	})
+}
+
+func TestURLStoreImpl_DeleteURL(t *testing.T) {
+	t.Run("WHEN existing static url deleted", func(t *testing.T) {
+		store := buildStore(t)
+
+		require.Equal(t, true, store.DeleteURL(6))
+
+		id, varMap := store.GetURL("/gopher/doc.png")
+		require.Equal(t, -1, id)
+		require.Empty(t, varMap)
+
+		require.Equal(t, false, store.DeleteURL(6))
+
+		require.Equal(t, 8, store.Count())
+	})
+}
+
+func buildStore(t *testing.T) URLStore {
+	t.Helper()
+
+	pairs := []struct {
+		id         int
+		key, value string
+	}{
+		{6, "/gopher/doc.png", "6"},
+		{7, "/gopher/doc", "7"},
+		{8, "/users/<id>", "8"},
+		{9, "/users/<id>/profile", "9"},
+		{10, "/users/<id>/<accnt:\\d+>/address", "10"},
+		{11, "/users/<id>/age", "11"},
+		{12, "/users/<id>/<accnt:\\d+>", "12"},
+		{13, "/users/<id>/test/<name>", "13"},
+		{14, "/users/abc/<id>/<name>", "14"},
+	}
+
+	store := newURLStoreTree()
+
+	for _, pair := range pairs {
+		store.Add(pair.id, pair.key, pair.value)
+	}
+
+	require.Equal(t, 9, store.Count())
+
+	return &URLStoreImpl{store: store}
+}
