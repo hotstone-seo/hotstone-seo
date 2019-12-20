710fc83a8d6f897fe44f331f0aeaac9c49532f9d
hendri <hendri.chia@tiket.com>
2019-12-20 14:11:02 +0700

    declare function data source list

--- src/views/DataSource/DataSource.js
+++ src/views/DataSource/DataSource.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardBody, CardHeader, Col, Pagination, PaginationItem, PaginationLink, Table, Button, NavLink } from 'reactstrap';
 import PropTypes from 'prop-types';
+import DataSourceForm from './DataSourceForm';
 
 class DataSource extends Component {
   constructor(props) {
@@ -22,15 +23,111 @@ class DataSource extends Component {
     this.handleEdit = this.handleEdit.bind(this);
 
   }
-  handleAdd() {
-    const { history } = this.props;
-    history.push('/dataSourceForm');
+  toggle() {
+    this.setState({
+      modal: !this.state.modal,
+    });
   }
-  handleEdit() {
-    const { history } = this.props;
-    history.push('/dataSourceEditForm');
+  toggleWarning() {
+    this.setState({
+      warning: !this.state.warning,
+    });
   }
+  getDataSourceList() {
+    axios.get(this.state.URL_API)
+      .then((res) => {
+        const datasources = res.data;
+        this.setState({ datasources });
+      }).catch((error) => {
+
+      });
+  }
+  componentDidMount() {
+    this.getDataSourceList();
+  }
+  handleDelete(id) {
+    axios.delete(this.state.URL_API + `/${id}`)
+      .then(() => {
+        const { datasources } = this.state;
+        this.setState({ datasources: datasources.filter((rul) => rul.id !== id) });
+      })
+      .catch((error) => {
+
+      });
+    this.toggleWarning()
+  }
+  showForm(record) {
+    if (record !== undefined) {
+      this.setState({ record: record });
+      this.setState({ actionForm: "Edit" });
+    }
+    else {
+      this.setState({ record: {} });
+      this.setState({ actionForm: "Add" });
+    }
+    this.setState({ formVisible: true });
+  }
+
+  saveFormRef(formRef) {
+    this.formRef = formRef;
+  }
+  handleCancel() {
+    this.setState({ formVisible: false });
+  }
+
+  handleSave() {
+    const { datasourcesFormValues, rules, actionForm, record } = this.state;
+    const isUpdate = actionForm !== "Add";
+
+    datasourcesFormValues.id = record.id;
+
+    if (isUpdate) {
+      axios.put(this.state.URL_API, datasourcesFormValues)
+        .then(() => {
+          const index = rules.findIndex((rul) => rul.id === record.id);
+          if (index > -1) {
+            rules[index] = datasourcesFormValues;
+            this.setState({ rules });
+          }
+        })
+        .then(() => {
+          this.getDataSourceList();
+        })
+        .catch((error) => {
+          console.log(error.message)
+        });
+    }
+    else {
+      axios.post(this.state.URL_API, datasourcesFormValues)
+        .then((response) => {
+          this.setState({ rules: [...datasources, datasourcesFormValues] });
+        })
+        .then(() => {
+          this.getDataSourceList();
+        })
+        .catch((error) => {
+
+        });
+      this.setState({ datasourcesFormValuess: {} });
+    }
+    this.setState({ formVisible: false });
+  }
+
+  handleOnChange(type, e) {
+    const { target } = e || {};
+    const { value } = target || {};
+    const { datasourcesFormValues } = this.state;
+
+    this.setState({
+      datasourcesFormValues: {
+        ...datasourcesFormValuess,
+        [type]: value
+      }
+    });
+  }
+
   render() {
+    const { datasources } = this.state;
     return (
       <div className="animated fadeIn">
         <Col xs="12" lg="12">
