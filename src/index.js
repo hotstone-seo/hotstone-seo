6b8edf399e6dcdfc1119aba0d32b863cbdf3f04d
Hawari Rahman <hawari.rahman17@gmail.com>
2019-12-23 10:53:49 +0700

    Create tag related methods

--- src/index.js
+++ src/index.js
@@ -1,38 +1,59 @@
 import React from 'react';
 import axios from 'axios';
 
+/**
+ * A client that is used as the main building blocks for interacting with HotStone provider.
+ * @typedef {Object} HotStoneClient
+ */
+
+/**
+ * Create an instance of HotStoneClient.
+ * 
+ * @param {string} host - URL of the HotStone provider
+ * @return {HotStoneClient} 
+ */
 function HotStone(host) {
-  const client = axios.create({ baseURL: `${host}/tags` });
-  return {
-    tags(path) {
+  const apiCaller = axios.create({ baseURL: host });
+  const client = {
+    match: function(path) {
       const context = {
-        async _realize() {
+        async _getRule() {
           try {
-            const { data } = await client.get(path);
+            const { data } = await apiCaller.post('/provider/matchRule', { path });
             return data;
           } catch (error) {
-            console.log('error: ', error);
             return undefined;
           }
         },
-        async get() {
-          if (this.cache === undefined) {
-            this.cache = await this._realize();
+        async rule() {
+          if (this.rule === undefined) {
+            this.rule = await this._getRule();
           }
-          return this.cache;
+          return this.rule;
         },
-        async renderElement() {
-          const tags = await this.get();
-          return tags.map(({ type, props, children }) => (
+        async tags() {
+          try {
+            const { data } = await apiCaller.get('/provider/tags', { params: { ruleID: this.rule().id } });
+            return data;
+          } catch (error) {
+            return undefined;
+          }
+        },
+        renderTags(data) {
+          const tags = data.filter((element) => element.type === 'tags');
+          return this._render(tags);
+        },
+        _render(data) {
+          return data.map(({ type, props, children }) => (
             React.createElement(type, props, children)
           ));
         },
         // TODO add function for validation rule pattern against existing values
       };
-      
       return context;
     }
   }
+  return client;
 }
 
 export default HotStone;
