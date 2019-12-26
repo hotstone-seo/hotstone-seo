48ec705aa485c4fac1ae92694b7506fe12a1e830
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2019-12-26 10:52:29 +0700

    fix migration: urlstore_sync.rule NOT NULL

--- scripts/db/migration/2_urlstore_sync.up.sql
+++ scripts/db/migration/2_urlstore_sync.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE urlstore_sync (
     "version" serial PRIMARY KEY,
     operation TEXT NOT NULL,
-    rule_id INTEGER,
+    rule_id INTEGER NOT NULL,
     latest_url_pattern TEXT,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
