8383f9b36a2d10c8ce28cc3fb1e09d0625977d83
iman tung <iman.tung@gmail.com>
2019-12-30 18:07:50 +0700

    remove user and notification as not needed yet

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,11 +1,4 @@
 import React, { Component } from "react";
-import {
-  UncontrolledDropdown,
-  DropdownItem,
-  DropdownMenu,
-  DropdownToggle,
-  Nav
-} from "reactstrap";
 import PropTypes from "prop-types";
 
 import { AppNavbarBrand, AppSidebarToggler } from "@coreui/react";
@@ -19,36 +12,12 @@ const defaultProps = {};
 
 class DefaultHeader extends Component {
   render() {
-    // eslint-disable-next-line
-    const { children, ...attributes } = this.props;
-
     return (
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
           full={{ src: logo, height: 32, alt: "HotStone Logo" }}
         />
-
-        <Nav className="ml-auto" navbar>
-          <UncontrolledDropdown nav direction="down">
-            <DropdownToggle nav>
-              <img
-                src={"../../assets/img/avatars/6.jpg"}
-                className="img-avatar"
-                alt=""
-              />
-            </DropdownToggle>
-            <DropdownMenu right>
-              <DropdownItem>
-                <i className="fa fa-user"></i> Profile
-              </DropdownItem>
-
-              <DropdownItem onClick={e => this.props.onLogout(e)}>
-                <i className="fa fa-lock"></i> Logout
-              </DropdownItem>
-            </DropdownMenu>
-          </UncontrolledDropdown>
-        </Nav>
       </React.Fragment>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,54 +1,85 @@
-import React from 'react';
-import MetatagForm from './views/Metatag/MetatagForm';
+import React from "react";
+import MetatagForm from "./views/Metatag/MetatagForm";
 
-const Dashboard = React.lazy(() => import('./views/Dashboard'));
-const Users = React.lazy(() => import('./views/Users/Users'));
-const User = React.lazy(() => import('./views/Users/User'));
-const RuleList = React.lazy(() => import('./views/Rule/RuleList'));
-const RuleForm = React.lazy(() => import('./views/Rule/RuleForm'));
-const RuleDetail = React.lazy(() => import('./views/Rule/RuleDetail'));
-const MetaTagList = React.lazy(() => import('./views/Metatag/Metatag'));
-const MetatagPreview = React.lazy(() => import('./views/Metatag/MetatagPreview'));
-const MetaTagEditForm = React.lazy(() => import('./views/Metatag/MetatagEditForm'));
-const DataSourceList = React.lazy(() => import('./views/DataSource/DataSource'));
-const DataSourceForm = React.lazy(() => import('./views/DataSource/DataSourceForm'));
-const CanonicalList = React.lazy(() => import('./views/Canonical/Canonical'));
-const CanonicalForm = React.lazy(() => import('./views/Canonical/CanonicalForm'));
-const CanonicalEditForm = React.lazy(() => import('./views/Canonical/CanonicalEditForm'));
-const TitletagList = React.lazy(() => import('./views/Titletag/Titletag'));
-const TitletagForm = React.lazy(() => import('./views/Titletag/TitletagForm'));
-const TitletagEditForm = React.lazy(() => import('./views/Titletag/TitletagEditForm'));
-const ScripttagList = React.lazy(() => import('./views/Scripttag/Scripttag'));
-const ScripttagForm = React.lazy(() => import('./views/Scripttag/ScripttagForm'));
-const ScripttagEditForm = React.lazy(() => import('./views/Scripttag/ScripttagEditForm'));
-const LanguageList = React.lazy(() => import('./views/Language/Language'));
-const LanguageForm = React.lazy(() => import('./views/Language/LanguageForm'));
+const Dashboard = React.lazy(() => import("./views/Dashboard"));
+const RuleList = React.lazy(() => import("./views/Rule/RuleList"));
+const RuleForm = React.lazy(() => import("./views/Rule/RuleForm"));
+const RuleDetail = React.lazy(() => import("./views/Rule/RuleDetail"));
+const MetaTagList = React.lazy(() => import("./views/Metatag/Metatag"));
+const MetatagPreview = React.lazy(() =>
+  import("./views/Metatag/MetatagPreview")
+);
+const MetaTagEditForm = React.lazy(() =>
+  import("./views/Metatag/MetatagEditForm")
+);
+const DataSourceList = React.lazy(() =>
+  import("./views/DataSource/DataSource")
+);
+const DataSourceForm = React.lazy(() =>
+  import("./views/DataSource/DataSourceForm")
+);
+const CanonicalList = React.lazy(() => import("./views/Canonical/Canonical"));
+const CanonicalForm = React.lazy(() =>
+  import("./views/Canonical/CanonicalForm")
+);
+const CanonicalEditForm = React.lazy(() =>
+  import("./views/Canonical/CanonicalEditForm")
+);
+const TitletagList = React.lazy(() => import("./views/Titletag/Titletag"));
+const TitletagForm = React.lazy(() => import("./views/Titletag/TitletagForm"));
+const TitletagEditForm = React.lazy(() =>
+  import("./views/Titletag/TitletagEditForm")
+);
+const ScripttagList = React.lazy(() => import("./views/Scripttag/Scripttag"));
+const ScripttagForm = React.lazy(() =>
+  import("./views/Scripttag/ScripttagForm")
+);
+const ScripttagEditForm = React.lazy(() =>
+  import("./views/Scripttag/ScripttagEditForm")
+);
+const LanguageList = React.lazy(() => import("./views/Language/Language"));
+const LanguageForm = React.lazy(() => import("./views/Language/LanguageForm"));
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/dashboard', name: 'Dashboard', component: Dashboard },
-  { path: '/users', exact: true,  name: 'Users', component: Users },
-  { path: '/users/:id', exact: true, name: 'User Details', component: User },
-  { path: '/rule', name: 'Rules', component: RuleList },
-  { path: '/ruleForm', name: 'Rules', component: RuleForm },
-  { path: '/ruleDetail?ruleId=:id', exact: true, name: 'Rule Details', component: RuleDetail },
-  { path: '/metatag', name: 'Metatag', component: MetaTagList },
-  { path: '/metatagForm', name: 'Metatag', component: MetatagForm },
-  { path: '/metatagEditForm', name: 'Metatag', component: MetaTagEditForm },
-  { path: '/metatagPreview', name: 'MetatagPreview', component: MetatagPreview },
-  { path: '/datasource', name: 'DataSource', component: DataSourceList },
-  { path: '/dataSourceForm', name: 'DataSource', component: DataSourceForm },
-  { path: '/canonical', name: 'Canonical', component: CanonicalList },
-  { path: '/canonicalForm', name: 'Canonical', component: CanonicalForm },
-  { path: '/canonicalEditForm', name: 'Canonical', component: CanonicalEditForm },
-  { path: '/titletag', name: 'Titletag', component: TitletagList },
-  { path: '/titletagForm', name: 'Titletag', component: TitletagForm },
-  { path: '/titletagEditForm', name: 'Titletag', component: TitletagEditForm },
-  { path: '/scripttag', name: 'Scripttag', component: ScripttagList },
-  { path: '/scripttagForm', name: 'Scripttag', component: ScripttagForm },
-  { path: '/scripttagEditForm', name: 'Scripttag', component: ScripttagEditForm },
-  { path: '/language', name: 'Language', component: LanguageList },
-  { path: '/languageForm', name: 'Language', component: LanguageForm },
+  { path: "/", exact: true, name: "Home" },
+  { path: "/dashboard", name: "Dashboard", component: Dashboard },
+  { path: "/rule", name: "Rules", component: RuleList },
+  { path: "/ruleForm", name: "Rules", component: RuleForm },
+  {
+    path: "/ruleDetail?ruleId=:id",
+    exact: true,
+    name: "Rule Details",
+    component: RuleDetail
+  },
+  { path: "/metatag", name: "Metatag", component: MetaTagList },
+  { path: "/metatagForm", name: "Metatag", component: MetatagForm },
+  { path: "/metatagEditForm", name: "Metatag", component: MetaTagEditForm },
+  {
+    path: "/metatagPreview",
+    name: "MetatagPreview",
+    component: MetatagPreview
+  },
+  { path: "/datasource", name: "DataSource", component: DataSourceList },
+  { path: "/dataSourceForm", name: "DataSource", component: DataSourceForm },
+  { path: "/canonical", name: "Canonical", component: CanonicalList },
+  { path: "/canonicalForm", name: "Canonical", component: CanonicalForm },
+  {
+    path: "/canonicalEditForm",
+    name: "Canonical",
+    component: CanonicalEditForm
+  },
+  { path: "/titletag", name: "Titletag", component: TitletagList },
+  { path: "/titletagForm", name: "Titletag", component: TitletagForm },
+  { path: "/titletagEditForm", name: "Titletag", component: TitletagEditForm },
+  { path: "/scripttag", name: "Scripttag", component: ScripttagList },
+  { path: "/scripttagForm", name: "Scripttag", component: ScripttagForm },
+  {
+    path: "/scripttagEditForm",
+    name: "Scripttag",
+    component: ScripttagEditForm
+  },
+  { path: "/language", name: "Language", component: LanguageList },
+  { path: "/languageForm", name: "Language", component: LanguageForm }
 ];
 
 export default routes;
