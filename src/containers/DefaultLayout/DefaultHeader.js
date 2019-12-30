8d98c75fab0a5c98d9652c3feba410c337b80219
iman tung <iman.tung@gmail.com>
2019-12-30 18:01:41 +0700

    change log

--- src/containers/DefaultLayout/DefaultHeader.js
+++ src/containers/DefaultLayout/DefaultHeader.js
@@ -1,48 +1,54 @@
-import React, { Component } from 'react';
-import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav } from 'reactstrap';
-import PropTypes from 'prop-types';
+import React, { Component } from "react";
+import {
+  UncontrolledDropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Nav
+} from "reactstrap";
+import PropTypes from "prop-types";
 
-import { AppNavbarBrand, AppSidebarToggler } from '@coreui/react';
-import logo from '../../assets/img/brand/hotstoneseo_header.png'
-import hostoneseo_logo from '../../assets/img/brand/hotstoneseo_logo.png'
+import { AppNavbarBrand, AppSidebarToggler } from "@coreui/react";
+import logo from "../../assets/img/hotstone-seo.png";
 
 const propTypes = {
-  children: PropTypes.node,
+  children: PropTypes.node
 };
 
 const defaultProps = {};
 
 class DefaultHeader extends Component {
   render() {
-
     // eslint-disable-next-line
     const { children, ...attributes } = this.props;
 
     return (
-    
       <React.Fragment>
         <AppSidebarToggler className="d-lg-none" display="md" mobile />
         <AppNavbarBrand
-          full={{ src: logo, width: 89, height: 21, alt: 'HotStone Logo' }}
-          minimized={{ src: hostoneseo_logo, width: 30, height: 30, alt: 'HotStone Logo' }}
+          full={{ src: logo, height: 32, alt: "HotStone Logo" }}
         />
-        <AppSidebarToggler className="d-md-down-none" display="lg" />
-  
+
         <Nav className="ml-auto" navbar>
-         
           <UncontrolledDropdown nav direction="down">
             <DropdownToggle nav>
-              <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="" />
+              <img
+                src={"../../assets/img/avatars/6.jpg"}
+                className="img-avatar"
+                alt=""
+              />
             </DropdownToggle>
             <DropdownMenu right>
-     
-              <DropdownItem><i className="fa fa-user"></i> Profile</DropdownItem>
-              
-              <DropdownItem onClick={e => this.props.onLogout(e)}><i className="fa fa-lock"></i> Logout</DropdownItem>
+              <DropdownItem>
+                <i className="fa fa-user"></i> Profile
+              </DropdownItem>
+
+              <DropdownItem onClick={e => this.props.onLogout(e)}>
+                <i className="fa fa-lock"></i> Logout
+              </DropdownItem>
             </DropdownMenu>
           </UncontrolledDropdown>
         </Nav>
-       
       </React.Fragment>
     );
   }
