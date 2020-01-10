6efbbb14c74c4d43557d07dc4886f14ecc2fa7a7
hendri <hendri.chia@tiket.com>
2020-01-10 16:39:44 +0700

    db : remove sign " query insert tag

--- scripts/db/seed/tags.sql
+++ scripts/db/seed/tags.sql
@@ -1,4 +1,4 @@
-INSERT INTO tags (id, rule_id, "locale", "type", attributes, "value")
+INSERT INTO tags (id, rule_id, locale, "type", attributes, "value")
 VALUES 
     (1, 1, 'EN', 'title', '{}', 'Airport Title'),
     (2, 1, 'EN', 'meta', '{"type": "description", "content": "Airport Description"}', '');
