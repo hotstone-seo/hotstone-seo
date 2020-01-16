003e6681c6a4aa7f24e450269114656bff1304e1
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-01-16 11:52:43 +0700

    wip: HitChartCard

--- ui/src/views/Analytic/HitChartCard.js
+++ ui/src/views/Analytic/HitChartCard.js
@@ -1,15 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useReducer } from "react";
+import { ResponsiveLine } from "@nivo/line";
 import useHotstoneAPI from "../../hooks/useHotstoneAPI";
 import useInterval from "@use-it/interval";
+import { useForm } from "react-hook-form";
+import { format, subDays, startOfMonth } from "date-fns";
+import dataChart from "./data";
+import inspectAxiosError, { isAxiosError } from "../../utils/axios";
+
+const DATE_FORMAT = "yyyy-MM-dd";
 
 const LAST_7_DAYS = "last7Days";
 const THIS_MONTH = "thisMonth";
 
 const initialState = {
-  selectedOption: "",
-  startDate: null,
-  endDate: null
+  selectedOption: LAST_7_DAYS,
+  startDate: format(subDays(new Date(), 7), DATE_FORMAT),
+  endDate: format(new Date(), DATE_FORMAT)
 };
+
+function reducer(state, action) {
+  const now = new Date();
+  switch (action.type) {
+    case LAST_7_DAYS:
+      return {
+        selectedOption: LAST_7_DAYS,
+        startDate: format(subDays(now, 7), DATE_FORMAT),
+        endDate: format(now, DATE_FORMAT)
+      };
+    case THIS_MONTH:
+      return {
+        selectedOption: THIS_MONTH,
+        startDate: format(startOfMonth(now), DATE_FORMAT),
+        endDate: format(now, DATE_FORMAT)
+      };
+    default:
+      throw new Error();
+  }
+}
 
 const rangeOptions = [
   { value: LAST_7_DAYS, label: "Last 7 Days" },
@@ -17,27 +44,40 @@ const rangeOptions = [
 ];
 
 function HitChartCard() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const { register, handleSubmit, errors } = useForm();
-  const onChangeRange = data => console.log(data);
 
   const [countHit, setCountHit] = useState(0);
-  const [{ data: dataCountHit }, refetch] = useHotstoneAPI({
-    url: "metrics/hit"
+  const [{ data: dataListCountHit, error }, refetch] = useHotstoneAPI({
+    url: `metrics/hit/range?start=${state.startDate}&end=${state.endDate}`
   });
 
+  // inspectAxiosError(error);
+  const onChangeRange = selectedOption => {
+    console.log(selectedOption);
+    dispatch({ type: selectedOption.range });
+    refetch();
+  };
+
   useEffect(() => {
-    if (dataCountHit !== undefined) {
-      setCountHit(dataCountHit.count);
+    if (dataListCountHit !== undefined) {
+      setCountHit(dataListCountHit.count);
     }
-  }, [dataCountHit]);
+  }, [dataListCountHit]);
 
   useInterval(() => {
     refetch();
   }, 5_000);
 
   return (
-    <div className="card" style={{ height: 400 }}>
+    <div className="card">
       <div className="card-header text-left">
+        {isAxiosError(error) && (
+          <div class="alert alert-danger" role="alert">
+            Failed to fetch data
+          </div>
+        )}
         <form>
           <select
             name="range"
@@ -45,12 +85,16 @@ function HitChartCard() {
             onChange={handleSubmit(onChangeRange)}
           >
             {rangeOptions.map(({ value, label }, index) => {
-              return <option value={value}>{label}</option>;
+              return (
+                <option key={value} value={value}>
+                  {label}
+                </option>
+              );
             })}
           </select>
         </form>
       </div>
-      <div className="card-body">
+      <div className="card-body" style={{ height: 400 }}>
         <ResponsiveLine
           data={dataChart}
           margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
@@ -67,7 +111,7 @@ function HitChartCard() {
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: "transportation",
+            legend: "Date",
             legendOffset: 36,
             legendPosition: "middle"
           }}
