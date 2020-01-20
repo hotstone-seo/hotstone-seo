2f56fc9eb18f047fe55bd3a2e035e5b441becb2c
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-01-20 16:12:19 +0700

    fix seed: set latest value of urlstore_sync_version_seq

--- scripts/db/seed/urlstore_sync.sql
+++ scripts/db/seed/urlstore_sync.sql
@@ -3,3 +3,5 @@ VALUES
     (1, 'INSERT', 1, '/airports'),
     (2, 'INSERT', 2, '/hotels'),
     (3, 'INSERT', 3, '/events');
+
+SELECT setval('urlstore_sync_version_seq', (SELECT MAX(version) FROM urlstore_sync));
