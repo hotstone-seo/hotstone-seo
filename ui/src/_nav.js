11fb7f87ac73c4c8b415ff9b6430f515253380cd
hendri <hendri.chia@tiket.com>
2020-01-21 15:37:53 +0700

    sidebar : hide Locale menu

--- ui/src/_nav.js
+++ ui/src/_nav.js
@@ -6,11 +6,11 @@ export default {
       url: "/rule",
       icon: "fa fa-home"
     },
-    {
+    /*{
       name: "Language",
       url: "/language",
       icon: "fa fa-language"
-    },
+    },*/
     {
       name: "DataSource",
       url: "/datasource",
