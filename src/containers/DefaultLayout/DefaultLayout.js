52f81b0440b83ed4c5b382051b7948d0e09bbfa1
Hawari Rahman <hawari.rahman17@gmail.com>
2020-01-23 17:36:01 +0700

    Add basic logic for rendering Route

--- src/containers/DefaultLayout/DefaultLayout.js
+++ src/containers/DefaultLayout/DefaultLayout.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { Layout } from 'antd';
+import { Link, Route, Switch } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
+import routes from '../../routes';
 
 const { Header, Content, Sider } = Layout;
 
-function DefaultLayout() {
+function DefaultLayout(props) {
   return (
     <Layout>
-      <Header/>
+      <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }} />
       <Layout>
         <Sider>
+          <Menu>
+            {routes.map((route, idx) => (
+              <Menu.Item key={idx}>
+                <Link to={route.path}>{route.name}</Link>
+              </Menu.Item>
+            ))}
+          </Menu>
         </Sider>
         <Layout>
           <Content>
+            <Switch>
+              {routes.map((route, idx) => {
+                return route.component ? (
+                  <Route
+                    key={idx}
+                    path={route.path}
+                    exact={route.exact}
+                    name={route.name}
+                    render={props => <route.component {...props} />}
+                  />
+                ) : null;
+              })}
+            </Switch>
           </Content>
         </Layout>
       </Layout>
---
+++ src/views/Rule/Rule.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Rule(props) {
+  return (
+    <div>Rule page</div>
+  );
+}
+
+export default Rule;
