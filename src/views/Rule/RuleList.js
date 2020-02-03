e468527fc5a42d4d4c9b460d8dfc178727b703ee
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-03 12:38:32 +0700

    Add action links element to RuleList

--- src/views/Rule/RuleList.js
+++ src/views/Rule/RuleList.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Button, Divider } from 'antd';
 
 const columns = [
   { title: 'Name', dataIndex: 'name', key: 'name' },
   { title: 'URL Pattern', dataIndex: 'url_pattern', key: 'urlPattern' },
   { title: 'Last Updated', dataIndex: 'updated_at', key: 'lastUpdated' },
+  {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+        <Button type="link" style={{ padding: 0 }}>Edit</Button>
+        <Divider type="vertical" />
+        <Button type="link" danger style={{ padding: 0 }}>Delete</Button>
+      </span>
+    )
+  }
 ]
 
 function RuleList({ rules }) {
