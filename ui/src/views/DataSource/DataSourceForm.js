0607abf1ddfb5b7332f385f8a314a62323564add
hendri <hendri.chia@tiket.com>
2020-02-03 09:40:31 +0700

    ui :  add  * for mandatory field

--- ui/src/views/DataSource/DataSourceForm.js
+++ ui/src/views/DataSource/DataSourceForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 import {
   Button,
@@ -10,54 +10,89 @@ import {
   Modal,
   ModalHeader,
   ModalBody,
-  ModalFooter,
-} from 'reactstrap';
+  ModalFooter
+} from "reactstrap";
 
 class DataSourceForm extends Component {
   render() {
     const {
-      visible, onCancel, onSave, datasource, action, onChange
+      visible,
+      onCancel,
+      onSave,
+      datasource,
+      action,
+      onChange
     } = this.props;
     return (
       <Modal isOpen={visible}>
         <ModalHeader>{action} Data Source</ModalHeader>
         <ModalBody>
-          <Form action="" method="post" encType="multipart/form-data" className="form-horizontal">
+          <Form
+            action=""
+            method="post"
+            encType="multipart/form-data"
+            className="form-horizontal"
+          >
             <FormGroup row>
-              {datasource !== undefined ? (<Input type="hidden" id="id" name="id" defaultValue={datasource.id} onChange={onChange.bind(this, 'id')} />) : ""}
+              {datasource !== undefined ? (
+                <Input
+                  type="hidden"
+                  id="id"
+                  name="id"
+                  defaultValue={datasource.id}
+                  onChange={onChange.bind(this, "id")}
+                />
+              ) : (
+                ""
+              )}
               <Col md="3">
-                <Label htmlFor="text-input">Name</Label>
+                <Label htmlFor="text-input">
+                  Name <span className="mandatory-field">*</span>
+                </Label>
               </Col>
               <Col xs="12" md="9">
-                <Input type="text" 
-                id="name" 
-                name="name" 
-                placeholder="Name" 
-                onChange={onChange.bind(this, 'name')} 
-                defaultValue={datasource !== undefined?datasource.name:""}
+                <Input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Name"
+                  onChange={onChange.bind(this, "name")}
+                  defaultValue={datasource !== undefined ? datasource.name : ""}
                 />
               </Col>
             </FormGroup>
             <FormGroup row>
               <Col md="3">
-                <Label htmlFor="textarea-input">URL</Label>
+                <Label htmlFor="textarea-input">
+                  URL <span className="mandatory-field">*</span>
+                </Label>
               </Col>
               <Col xs="12" md="9">
-                <Input type="textarea" 
-                name="url" 
-                id="url" 
-                rows="3" 
-                placeholder="URL" 
-                onChange={onChange.bind(this, 'url')} 
-                defaultValue={datasource !== undefined?datasource.url:""}
+                <Input
+                  type="textarea"
+                  name="url"
+                  id="url"
+                  rows="3"
+                  placeholder="URL"
+                  onChange={onChange.bind(this, "url")}
+                  defaultValue={datasource !== undefined ? datasource.url : ""}
                 />
+              </Col>
+            </FormGroup>
+            <FormGroup row>
+              <Col xs="12" md="9">
+                <span className="mandatory-field">* : must be filled in</span>
               </Col>
             </FormGroup>
           </Form>
         </ModalBody>
         <ModalFooter>
-          <Button color="warning" onClick={onSave}>Save</Button>{' '}
-          <Button color="secondary" onClick={onCancel}>Cancel</Button>
+          <Button color="warning" onClick={onSave}>
+            Save
+          </Button>{" "}
+          <Button color="secondary" onClick={onCancel}>
+            Cancel
+          </Button>
         </ModalFooter>
       </Modal>
     );
