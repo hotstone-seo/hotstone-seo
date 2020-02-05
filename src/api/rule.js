940a4751fb14067c63b6f85602e07f292881a33a
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-05 13:30:52 +0700

    Update API functions

--- src/api/rule.js
+++ src/api/rule.js
@@ -8,4 +8,46 @@ function fetchRules() {
                });
 }
 
-export { fetchRules };
+function getRule(id) {
+  return client.get(`/rules/${id}`)
+               .then(response => response.data)
+               .catch(error => {
+                 throw error;
+               });
+}
+
+function createRule(rule) {
+  return client.post('/rules', rule)
+               .then(response => response.data)
+               .catch(error => {
+                 throw error;
+               });
+}
+
+function updateRule(rule) {
+  return client.put('/rules', rule)
+               .then(response => response.data)
+               .catch(error => {
+                 throw error;
+               });
+}
+
+function deleteRule(id) {
+  return client.delete(`/rules/${id}`)
+               .then(response => response.data)
+               .catch(error => {
+                 throw error;
+               });
+}
+
+export { fetchRules, getRule, createRule, updateRule, deleteRule };
+
+const RuleAPI = {
+  fetch: fetchRules,
+  get: getRule,
+  create: createRule,
+  update: updateRule,
+  delete: deleteRule,
+}
+
+export default RuleAPI;
