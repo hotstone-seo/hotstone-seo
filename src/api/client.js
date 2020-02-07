7d93e109e2d4e76f193a92f10c8165574cf175e4
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-07 13:59:29 +0700

    Add custom error handling in axios

--- src/api/client.js
+++ src/api/client.js
@@ -1,5 +1,20 @@
 import axios from 'axios';
 
-export default axios.create({
+const client = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
+
+client.interceptors.response.use(
+  (response) => {
+    return response;
+  }, (error) => {
+    console.error(error);
+    if (error.response) {
+      const { data: { message } } = error.response;
+      return Promise.reject(new Error(message || "Unexpected error occured in server"));
+    }
+    return Promise.reject(error);
+  }
+)
+
+export default client;
