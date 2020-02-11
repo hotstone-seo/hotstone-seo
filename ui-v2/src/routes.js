1f44d2fb2e7c08373a7e7b252702a6e09f94a233
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-11 15:17:38 +0700

    Remove TagList from routes

--- ui-v2/src/routes.js
+++ ui-v2/src/routes.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 const Rule = React.lazy(() => import('./views/Rule'));
 
-const TagList = React.lazy(() => import('./components/Tag/TagList'));
-
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/rules', name: 'Rules', component: Rule },
-  { path: '/tags', name: 'Tags', component: TagList }
 ]
 
 export default routes;
