05e44d0339e0ce54bf06472d6f2ce3ae94fa4f78
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-11 11:30:48 +0700

    Moving all files and directory to one folder for merging

---
+++ ui-v2/src/components/Tag/TagForm.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Form, Input } from 'antd';
+
+function TagForm() {
+  const [form] = Form.useForm();
+  
+  return (
+    <Form form={form}>
+    </Form>
+  )
+}
+
+export default TagForm;
