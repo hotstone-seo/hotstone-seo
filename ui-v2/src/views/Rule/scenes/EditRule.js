3794efbf9a563fc30628634b1def0c3bc94beae2
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-12 17:16:14 +0700

    Set locale filter on EditRule

--- ui-v2/src/views/Rule/scenes/EditRule.js
+++ ui-v2/src/views/Rule/scenes/EditRule.js
@@ -1,19 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-import { Row, Col, message } from 'antd';
+import { Row, Col, message, Select } from 'antd';
 import { RuleForm } from 'components/Rule';
 import { TagList } from 'components/Tag';
 import { getRule, updateRule } from 'api/rule';
 import { fetchTags } from 'api/tag';
+import locales from 'locales';
 import styles from './AddRule.module.css';
+
+const { Option } = Select;
 
 function EditRule() {
   const { id } = useParams();
   const history = useHistory();
 
   const [rule, setRule] = useState({});
-
   const [tags, setTags] = useState([]);
+  const [locale, setLocale] = useState(locales[0] || '');
 
   useEffect(() => {
     getRule(id)
@@ -24,12 +27,12 @@ function EditRule() {
   }, [id]);
 
   useEffect(() => {
-    fetchTags({ rule_id: id })
+    fetchTags({ rule_id: id, locale: locale })
       .then(tags => { setTags(tags) })
       .catch(error => {
         message.error(error.message);
       });
-  }, [id])
+  }, [id, locale])
 
   const handleEdit = (newRule) => {
     newRule.id = rule.id;
@@ -42,6 +45,10 @@ function EditRule() {
       });
   };
 
+  const changeLocale = (newLocale) => {
+    setLocale(newLocale);
+  };
+
   return (
     <div>
       <Row>
@@ -51,6 +58,11 @@ function EditRule() {
       </Row>
       <Row style={{ marginTop: 24 }}>
         <Col className={styles.container} span={16} style={{ padding: 24 }}>
+          <Select defaultValue={locale} onChange={changeLocale}>
+            {locales.map(locale => (
+              <Option value={locale}>{locale}</Option>
+            ))}
+          </Select>
           <TagList tags={tags} />
         </Col>
       </Row>
