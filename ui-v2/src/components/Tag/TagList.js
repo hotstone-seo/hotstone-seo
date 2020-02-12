e03aa850925a8cb6154b652f8ec42d3f0aa32a30
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-12 17:16:31 +0700

    Allow edit and delete behavior to be injected

--- ui-v2/src/components/Tag/TagList.js
+++ ui-v2/src/components/Tag/TagList.js
@@ -2,31 +2,39 @@ import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { List, Button } from 'antd';
 
-// TODO: Make sure that tag with no children like <meta /> don't have
-// "value" key in its object or has null or undefined as its value.
-// It interferes with React's renderToStaticMarkup function
-// which will create an empty children whenever the value key is set
-// to empty string.
-function TagList(props) {
-  const tags = props.tags.map(tag => {
+function TagList({ tags, onEdit, onDelete }) {
+  const sanitizeTag = (tag) => {
     if (tag.type === 'meta') {
-      tag.value = null
+      tag.value = null;
     }
     return tag;
-  });
+  }
   return (
     <List
-      dataSource={tags}
-      renderItem={({ type, attributes, value }) => (
+      dataSource={tags.map(sanitizeTag)}
+      renderItem={(tag) => (
         <List.Item
           actions={[
-            <Button type="link" style={{ padding: 0 }}>Edit</Button>,
-            <Button type="link" danger style={{ padding: 0 }}>Delete</Button>
+            <Button
+              type="link"
+              onClick={() => onEdit(tag)}
+              style={{ padding: 0 }}
+            >
+              Edit
+            </Button>,
+            <Button
+              type="link"
+              danger
+              onClick={() => onDelete(tag)}
+              style={{ padding: 0 }}
+            >
+              Delete
+            </Button>
           ]}
         >
           <pre>
             {ReactDOMServer.renderToStaticMarkup(
-              React.createElement(type, attributes, value)
+              React.createElement(tag.type, tag.attributes, tag.value)
             )}
           </pre>
         </List.Item>
