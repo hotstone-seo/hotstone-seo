0534674990396f07d460dafcd2ccdfb80f641b58
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-02-17 10:06:33 +0700

    scenes/ViewRules: use RuleListV2

--- ui-v2/src/components/Rule/index.js
+++ ui-v2/src/components/Rule/index.js
@@ -1,5 +1,6 @@
-import RuleDetail from './RuleDetail';
-import RuleForm from './RuleForm';
-import RuleList from './RuleList';
+import RuleDetail from "./RuleDetail";
+import RuleForm from "./RuleForm";
+import RuleList from "./RuleList";
+import RuleListV2 from "./RuleListV2";
 
-export { RuleDetail, RuleForm, RuleList };
+export { RuleDetail, RuleForm, RuleList, RuleListV2 };
