0f1c00c68d17911e39c8f6d531ca57ce73f9914a
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-02-18 14:07:08 +0700

    ui-v2: MismatchRule

--- ui-v2/src/routes.js
+++ ui-v2/src/routes.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
-const Rule = React.lazy(() => import('./views/Rule'));
+const Rule = React.lazy(() => import("./views/Rule"));
+const MismatchRule = React.lazy(() => import("./views/MismatchRule"));
 
 const routes = [
-  { path: '/', exact: true, name: 'Home' },
-  { path: '/rules', name: 'Rules', component: Rule },
-]
+  { path: "/", exact: true, name: "Home" },
+  { path: "/rules", name: "Rules", component: Rule },
+  { path: "/mismatch-rule", name: "MismatchRule", component: MismatchRule }
+];
 
 export default routes;
---
+++ ui-v2/src/views/MismatchRule/scenes/index.js
@@ -0,0 +1,3 @@
+import ViewMismatchRules from "./ViewMismatchRules";
+
+export { ViewMismatchRules };
