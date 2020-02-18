a3120b78f62809b03190c6b3ce4accc59d34ac59
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-18 17:20:18 +0700

    Add PageHeader to ViewDataSources

--- ui-v2/src/views/DataSource/scenes/ViewDataSources.js
+++ ui-v2/src/views/DataSource/scenes/ViewDataSources.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, useHistory } from 'react-router-dom';
-import { Button, message } from 'antd';
+import { PageHeader, Button, message } from 'antd';
 import { deleteDataSource } from 'api/datasource';
 import useDataSources from 'hooks/useDataSources';
 import { DataSourceList } from 'components/DataSource';
@@ -27,15 +27,22 @@ function ViewDataSources({ match }) {
 
   return (
     <div>
-      <Button type="primary" style={{ marginBottom: 16 }}>
-        <Link to={`${match.url}/new`}>Add new Data Source</Link>
-      </Button>
-      <DataSourceList
-        dataSources={dataSources}
-        onClick={showEditScene}
-        onEdit={showEditScene}
-        onDelete={removeDataSource}
+      <PageHeader
+        title="Data Sources"
+        subTitle="Manage location for retrieving resources"
+        style={{ background: '#fff' }}
       />
+      <div style={{ padding: 24 }}>
+        <Button type="primary" style={{ marginBottom: 16 }}>
+          <Link to={`${match.url}/new`}>Add new Data Source</Link>
+        </Button>
+        <DataSourceList
+          dataSources={dataSources}
+          onClick={showEditScene}
+          onEdit={showEditScene}
+          onDelete={removeDataSource}
+        />
+      </div>
     </div>
   );
 }
