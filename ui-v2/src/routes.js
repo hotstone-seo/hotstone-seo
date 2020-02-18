c4fa55938561fbd39971e054196deba13bd1d722
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-18 17:11:52 +0700

    Adjust code style in routes.js

--- ui-v2/src/routes.js
+++ ui-v2/src/routes.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React from 'react';
 
-const Rule = React.lazy(() => import("./views/Rule"));
-const MismatchRule = React.lazy(() => import("./views/MismatchRule"));
-
+const Rule = React.lazy(() => import('./views/Rule'));
+const MismatchRule = React.lazy(() => import('./views/MismatchRule'));
 const DataSource = React.lazy(() => import('./views/DataSource'));
 
 const routes = [
-  { path: "/", exact: true, name: "Home" },
-  { path: "/rules", name: "Rules", component: Rule },
+  { path: '/', exact: true, name: 'Home' },
+  { path: '/rules', name: 'Rules', component: Rule },
   { path: '/datasources', name: 'Data Sources', component: DataSource },
-  { path: "/mismatch-rule", name: "Mismatch Rule", component: MismatchRule }
+  { path: '/mismatch-rule', name: 'Mismatch Rule', component: MismatchRule },
 ];
 
 export default routes;
