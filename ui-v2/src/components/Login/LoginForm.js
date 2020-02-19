bd5fc62f5988a783291090adb63a453dc72c0958
Hawari Rahman <hawari.rahman17@gmail.com>
2020-02-19 13:57:35 +0700

    Clear up separation of concern in Login components

--- ui-v2/src/components/Login/LoginForm.js
+++ ui-v2/src/components/Login/LoginForm.js
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
-import { Form, Input, Checkbox, Button } from 'antd';
-
-const formLayout = { labelCol: { span: 8 }, wrapperCol: { span: 16 } };
-
-const tailLayout = { wrapperCol: { offset: 8, span: 16 } };
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+  Form, Input, Checkbox, Button,
+} from 'antd';
 
 function LoginForm({ login }) {
-  const [loading, setLoading] = useState(false);
-
   const [form] = Form.useForm();
 
-  // TODO: Figure out a way to redirect. Tried conditional rendering, incorrect
-  // usage cause React to do setState operation on unmounted component
-  const onFinish = (values) => {
-    const { email, password } = values;
-    setLoading(true);
-    login(email, password)
-        .catch((error) => {
-          setLoading(false);
-        });
-  };
-
   return (
-    <Form {...formLayout} form={form} onFinish={onFinish}>
+    <Form
+      form={form}
+      onFinish={login}
+      labelCol={{ span: 8 }}
+      wrapperCol={{ span: 16 }}
+    >
       <Form.Item
         label="Email"
         name="email"
@@ -39,17 +30,25 @@ function LoginForm({ login }) {
         <Input.Password />
       </Form.Item>
 
-      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+      <Form.Item
+        name="remember"
+        valuePropName="checked"
+        wrapperCol={{ offset: 8, span: 16 }}
+      >
         <Checkbox>Remember me</Checkbox>
       </Form.Item>
 
-      <Form.Item {...tailLayout}>
-        <Button type="primary" htmlType="submit" loading={loading}>
+      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+        <Button type="primary" htmlType="submit">
           Submit
         </Button>
       </Form.Item>
     </Form>
   );
 }
+
+LoginForm.propTypes = {
+  login: PropTypes.func.isRequired,
+};
 
 export default LoginForm;
