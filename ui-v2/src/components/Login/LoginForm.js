5f96e54aa75b2a837bb76dd40f27f6a558be3455
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-02-20 14:42:56 +0700

    LoginForm: add 'Sign in with Google' button

--- ui-v2/src/components/Login/LoginForm.js
+++ ui-v2/src/components/Login/LoginForm.js
@@ -1,54 +1,73 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {
-  Form, Input, Checkbox, Button,
-} from 'antd';
+import React from "react";
+import PropTypes from "prop-types";
+import { Form, Input, Checkbox, Button, Row, Divider, Typography } from "antd";
+import { GoogleOutlined } from "@ant-design/icons";
+import urljoin from "url-join";
+
+const { Text } = Typography;
 
 function LoginForm({ login }) {
   const [form] = Form.useForm();
 
   return (
-    <Form
-      form={form}
-      onFinish={login}
-      labelCol={{ span: 8 }}
-      wrapperCol={{ span: 16 }}
-    >
-      <Form.Item
-        label="Email"
-        name="email"
-        rules={[{ required: true, message: 'Please input your email!' }]}
+    <>
+      <Form
+        form={form}
+        onFinish={login}
+        labelCol={{ span: 8 }}
+        wrapperCol={{ span: 16 }}
       >
-        <Input />
-      </Form.Item>
+        <Form.Item
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "Please input your email!" }]}
+        >
+          <Input />
+        </Form.Item>
 
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[{ required: true, message: 'Please input your password!' }]}
-      >
-        <Input.Password />
-      </Form.Item>
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "Please input your password!" }]}
+        >
+          <Input.Password />
+        </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{ offset: 8, span: 16 }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
+        <Form.Item
+          name="remember"
+          valuePropName="checked"
+          wrapperCol={{ offset: 8, span: 16 }}
+        >
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
 
-      <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+          <Row>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+            <Divider type="vertical" />
+            <Text strong>OR</Text>
+            <Divider type="vertical" />
+            <Button
+              type="primary"
+              icon={<GoogleOutlined />}
+              href={urljoin(
+                process.env.REACT_APP_API_URL,
+                "/auth/google/login"
+              )}
+            >
+              Sign in with Google
+            </Button>
+          </Row>
+        </Form.Item>
+      </Form>
+    </>
   );
 }
 
 LoginForm.propTypes = {
-  login: PropTypes.func.isRequired,
+  login: PropTypes.func.isRequired
 };
 
 export default LoginForm;
