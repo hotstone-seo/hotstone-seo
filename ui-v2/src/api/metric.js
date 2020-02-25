7feb7e6a6d585acd9809559a45ebfd2d062c8b1b
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-02-25 22:12:05 +0700

    migrate Analytic to AntDesign

--- ui-v2/src/api/metric.js
+++ ui-v2/src/api/metric.js
@@ -9,10 +9,40 @@ function fetchMismatched(cfg = {}) {
     });
 }
 
-export { fetchMismatched };
+function fetchListCountHitPerDay(cfg = {}) {
+  return client
+    .get("metrics/hit/range", cfg)
+    .then(response => response.data)
+    .catch(error => {
+      throw error;
+    });
+}
+
+function fetchCountHit(cfg = {}) {
+  return client
+    .get("metrics/hit", cfg)
+    .then(response => response.data)
+    .catch(error => {
+      throw error;
+    });
+}
+
+function fetchCountUniquePage(cfg = {}) {
+  return client
+    .get("metrics/unique-page", cfg)
+    .then(response => response.data)
+    .catch(error => {
+      throw error;
+    });
+}
+
+export { fetchMismatched, fetchCountHit, fetchListCountHitPerDay, fetchCountUniquePage };
 
 const MetricAPI = {
-  fetchMismatched: fetchMismatched
+  fetchMismatched,
+  fetchCountHit,
+  fetchListCountHitPerDay,
+  fetchCountUniquePage
 };
 
 export default MetricAPI;
