8cf26bc9321645d0668eac38cc12aa9927525def
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-02-25 14:45:50 +0700

    setupProxy.js: "/auth/**" is also proxied

--- ui-v2/src/setupProxy.js
+++ ui-v2/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
-    createProxyMiddleware("/api/**", {
+    createProxyMiddleware(["/api/**", "/auth/**"], {
       target: "http://localhost:8089",
       changeOrigin: true
     })
