ff20927a97b5c8535aea31486518d945cd1a14fe
hendri <hendri.chia@tiket.com>
2020-02-27 11:03:27 +0700

    ui v1 : remove 'ui' folder

--- ui/src/hooks/useHotstoneAPI.js
+++
@@ -1,8 +0,0 @@
-import { makeUseAxios } from "axios-hooks";
-import axios from "axios";
-
-const useHotstoneAPI = makeUseAxios({
-  axios: axios.create({ baseURL: process.env.REACT_APP_API_URL })
-});
-
-export default useHotstoneAPI;
