c73453ac99317d5feed286345ea6fd126de5bee9
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-02-28 10:49:39 +0700

    eslintrc: allow pagination param field names for no-underscore-dangle rule

--- ui-v2/.eslintrc.js
+++ ui-v2/.eslintrc.js
@@ -4,6 +4,7 @@ module.exports = {
   extends: ['react-app', 'airbnb'],
   rules: {
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'no-underscore-dangle':  ['error', { 'allow': ['_sort', '_offset', '_limit', '_next_key', '_next'] }],
   },
   parser: "react-scripts/node_modules/babel-eslint",
   settings: {
