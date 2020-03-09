7c05b046f70d462eeb1f5c501369bdb0259e0a94
hendri <hendri.chia@tiket.com>
2020-03-09 18:59:53 +0700

    unit test. Still in progress

---
+++ ui-v2/src/views/DataSource/scenes/AddDataSource.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import {
+  render, fireEvent,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import mockAxios from 'jest-mock-axios';
+import AddDataSource from './AddDataSource';
+import { Form, Input } from "antd";
+
+const dataSource = [{
+  name: 'data source flight', url: 'localhost/flight',
+}];
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+const form = render(<AddDataSource />);
+const setup = () => {
+  const utils = form;
+  const input = utils.queryByText('Name').toBeInTheDocument();
+  return {
+    input,
+    ...utils,
+  };
+};
+
+describe('AddDataSource', () => {
+  test('name of data source', () => {
+    const { input } = setup();
+    fireEvent.change(input, { target: { value: 'data source airport' } });
+    expect(input.value).toBe('data source airport');
+  });
+
+  //test('save new data source', () => {
+  //  form.simulate('click');
+  //  expect(saveDataSource).toHaveBeenNthCalledWith(1, dataSource);
+  //});
+});
---
+++ ui-v2/src/views/Rule/scenes/ViewRules.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  render, wait,
+} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import mockAxios from 'jest-mock-axios';
+import ViewRules from './ViewRules';
+
+const respMock = [{
+  id: 1, name: 'Airport Rule', url: '/foo-ds', url_pattern: '/airport', updated_at: new Date().toISOString(), created_at: new Date().toISOString(),
+}];
+
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+describe('View Rules', () => {
+  test('first load', async () => {
+    const url = '/rules';
+    const {
+      queryByText,
+    } = render(<ViewRules match={{ url }} />);
+
+    await wait(() => {
+      expect(mockAxios.get).toHaveBeenCalledWith('/rules');
+      mockAxios.mockResponse({ data: respMock });
+
+      expect(queryByText(/Rules/)).toBeInTheDocument();
+      expect(queryByText(/Airport Rule/)).toBeInTheDocument();
+    });
+  });
+});
