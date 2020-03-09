4cc9c8226a346d5110f8b36fa4fc47f56861fbea
hendri <hendri.chia@tiket.com>
2020-03-10 05:48:26 +0700

    DB structure : add new field is_active

--- scripts/db/migration/1_rules.up.sql
+++ scripts/db/migration/1_rules.up.sql
@@ -3,6 +3,7 @@ CREATE TABLE rules (
     data_source_id INTEGER,
     "name" VARCHAR (255) NOT NULL,
     url_pattern TEXT NOT NULL UNIQUE,
+    is_active TINYINT DEFAULT '1',
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
