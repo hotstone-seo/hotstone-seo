7582d130bb1b53c1b5e8221b6b03f59a67512967
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-03-11 14:00:02 +0700

    package.json: setup eslint-plugin-cypress

--- ui-v2/.eslintrc.js
+++ ui-v2/.eslintrc.js
@@ -1,7 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  extends: ['react-app', 'airbnb'],
+  plugins: ["cypress"],
+  extends: ['react-app', 'airbnb', "plugin:cypress/recommended"],
   rules: {
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
     'no-underscore-dangle':  ['error', { 'allow': ['_sort', '_offset', '_limit', '_next_key', '_next'] }],
