333d3302c1358b00e6366ae266201f4e28c41296
Fahri Nurul Hidayat <fahri.hidayat@tiket.com>
2020-03-12 14:02:35 +0700

    AddRule.test.js: use userEvent

--- ui-v2/src/views/Rule/scenes/AddRule.test.js
+++ ui-v2/src/views/Rule/scenes/AddRule.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import {
-  cleanup, render, wait, fireEvent,
+  cleanup, render, wait,
 } from '@testing-library/react';
 
 import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
 import mockAxios from 'jest-mock-axios';
 import AddRule from './AddRule';
 
@@ -17,9 +18,7 @@ const respMock = [{
 describe('Add Rule', () => {
   test('good case', async () => {
     const {
-      queryByText,
       getByTestId,
-      debug,
     } = render(<AddRule />, { wrapper: MemoryRouter });
 
     expect(mockAxios.get).toHaveBeenCalledWith('/data_sources');
@@ -28,17 +27,17 @@ describe('Add Rule', () => {
     await wait();
 
     const nameInput = getByTestId('input-name');
-    fireEvent.change(nameInput, { target: { value: 'Rule ABC' } });
+    await userEvent.type(nameInput, 'Rule ABC');
     expect(nameInput.value).toBe('Rule ABC');
 
     const urlPatternInput = getByTestId('input-url-pattern');
-    fireEvent.change(urlPatternInput, { target: { value: '/abc' } });
+    await userEvent.type(urlPatternInput, '/abc');
     expect(urlPatternInput.value).toBe('/abc');
 
     await wait();
 
     const saveBtn = getByTestId('btn-save');
-    fireEvent.click(saveBtn);
+    userEvent.click(saveBtn);
 
     await wait();
 
