5ca4fe65e0e1baae8173ff20afe6c5b37d0d815c
hendri <hendri.chia@tiket.com>
2020-03-12 15:14:51 +0700

    remove unused code AddDataSource.test

--- ui-v2/src/views/DataSource/scenes/AddDataSource.test.js
+++ ui-v2/src/views/DataSource/scenes/AddDataSource.test.js
@@ -11,28 +11,8 @@ const dataSource = [{
   name: 'data source flight', url: 'localhost/flight',
 }];
 
-jest.mock('react-router-dom', () => ({
-  useHistory: () => ({
-    push: jest.fn(),
-  }),
-}));
-
-const form = render(<AddDataSource />);
-const setup = () => {
-  const utils = form;
-  const input = utils.queryByText('Name').toBeInTheDocument();
-  return {
-    input,
-    ...utils,
-  };
-};
 
 describe('AddDataSource', () => {
-  test('name of data source', () => {
-    const { input } = setup();
-    fireEvent.change(input, { target: { value: 'data source airport' } });
-    expect(input.value).toBe('data source airport');
-  });
 
   test('save new data source', () => {
   //  form.simulate('click');
