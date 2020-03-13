6ddcde71b5d8727563adc1b693f226d77e7f05d0
hendri <hendri.chia@tiket.com>
2020-03-13 17:40:29 +0700

    unit test handle delete data source

--- ui-v2/src/views/DataSource/scenes/ViewDataSources.test.js
+++ ui-v2/src/views/DataSource/scenes/ViewDataSources.test.js
@@ -4,6 +4,8 @@ import {
 } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import mockAxios from 'jest-mock-axios';
+import userEvent from '@testing-library/user-event';
+// import { createMemoryHistory } from 'history';
 import ViewDataSources from './ViewDataSources';
 
 const respMock = [{
@@ -31,4 +33,51 @@ describe('ViewDataSources', () => {
       expect(queryByText(/FooDS/)).toBeInTheDocument();
     });
   });
+
+  test('handle delete data source', async () => {
+    const url = '/datasources';
+    const {
+      getByTestId, queryByText,
+    } = render(<ViewDataSources match={{ url }} />);
+
+    await wait(() => {
+      expect(mockAxios.get).toHaveBeenCalledWith('/data_sources');
+      mockAxios.mockResponse({ data: respMock });
+
+      expect(queryByText(/FooDS/)).toBeInTheDocument();
+      // const firstRender = asFragment();
+      const saveBtn = getByTestId('btn-delete');
+      userEvent.click(saveBtn);
+
+      expect(saveBtn).toBeInTheDocument();
+      console.log('lolos', 'aa1');
+      // const history = createMemoryHistory({ initialEntries: ['/aa'] });
+      // userEvent.click(queryByText.getByText('Delete'));
+      // expect(history.location.search).toBe('?id=1');
+      // // expect(firstRender).toMatchDiffSnapshot(asFragment());
+      // expect(history.location.pathname).toBe('/aa');
+    });
+    // expect(mockAxios.delete).toHaveBeenCalledWith('/datasources', { id: 20 });
+    // console.log('lolos', 'aa2');
+  });
+
+  /*
+  test('handle edit data source', async () => {
+    const history = createMemoryHistory({ initialEntries: ['/'] });
+    const queryByText = render('<Link history={history}><Button>Edit</Button></Link>');
+    expect(queryByText(/Edit/)).toBeInTheDocument();
+    // userEvent.click(renderResult.getByText('Edit'));
+    expect(history.location.pathname).toBe('/');
+  });
+  */
+
+  // test('test aja', () => {
+  //   console.log('asik', 'aa');
+  //   const {
+  //     getByText,
+  //   } = render(<div><button type="button">Edit</button></div>);
+  //   expect(getByText('Edit')).toBeInTheDocument();
+  //   console.log('asik', 'aa');
+  // //     userEvent.click(saveBtn);
+  // });
 });
