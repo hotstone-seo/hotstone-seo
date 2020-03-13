b52566b41e5d8a3dc5f4585bd5a9998507418720
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-03-13 18:04:27 +0700

    rules pagination testing

--- ui-v2/cypress/integration/rules.spec.js
+++ ui-v2/cypress/integration/rules.spec.js
@@ -1,6 +1,56 @@
 describe('/rules', () => {
-  it('first load', () => {
-    cy.setCookie('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImZhaHJpLmhpZGF5YXRAdGlrZXQuY29tIiwiZXhwIjoxNTg0MDk3NTc3LCJwaWN0dXJlIjoiaHR0cHM6Ly9saDUuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1FNG5hTlVWMEo4by9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS93Y3ZMcXR6RnlpWS9waG90by5qcGcifQ.B2WoqwX6owiYv0d2fbRWn6fIk5MbjvIXlxVNdnvFl0E');
-    cy.visit('/rules');
+  beforeEach(() => {
+    cy.fixture('cookies.json').then((cookies) => {
+      cy.setCookie('token', cookies.token);
+    });
+
+    cy.server();
+
+    cy.fixture('rules/_offset0_limit10_total5.json').as('data_rules_offset0_limit10_total5');
+    cy.fixture('rules/_offset0_limit10_total8.json').as('data_rules_offset0_limit10_total8');
+    cy.fixture('rules/_offset5_limit10_total3.json').as('data_rules_offset5_limit10_total3');
+  });
+
+  context('pagination', () => {
+    it('GIVEN total <= pageSize THEN show only one page', () => {
+      cy.visit('/rules');
+
+      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@data_rules_offset0_limit10_total5')
+        .as('route_rules_offset0_limit10');
+
+      cy.wait('@route_rules_offset0_limit10');
+
+      cy.get('.ant-pagination-next > a').should('have.attr', 'disabled');
+    });
+
+    it('GIVEN total > pageSize THEN show two pages', () => {
+      cy.visit('/rules');
+
+      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@data_rules_offset0_limit10_total8')
+        .as('route_rules_offset0_limit10');
+
+      cy.wait('@route_rules_offset0_limit10');
+
+      cy.get('.ant-pagination-next > a').should('not.have.attr', 'disabled');
+    });
+
+    it('GIVEN two pages AND click page 2 THEN load page 2', () => {
+      cy.visit('/rules');
+
+      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@data_rules_offset0_limit10_total8')
+        .as('route_rules_offset0_limit10');
+
+      cy.wait('@route_rules_offset0_limit10');
+
+      cy.route('GET', '/api/rules?_offset=5&_limit=10', '@data_rules_offset5_limit10_total3')
+        .as('route_rules_offset5_limit10');
+
+      cy.get('.ant-pagination-next > a').click();
+
+      cy.wait('@route_rules_offset5_limit10');
+
+      cy.get('.ant-pagination-next > a').should('have.attr', 'disabled');
+      cy.get('[data-row-key]').should('have.length', 3);
+    });
   });
 });
