fac230825064312c59e2509e1a1a7fcc8721373a
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-03-17 17:50:21 +0700

    fixtures/rules: change file naming

--- ui-v2/cypress/integration/rules_pagination.spec.js
+++ ui-v2/cypress/integration/rules_pagination.spec.js
@@ -6,19 +6,19 @@ describe('rules_pagination', () => {
 
     cy.server();
 
-    cy.fixture('rules/_offset0_limit10_total5.json').as('data_rules_offset0_limit10_total5');
-    cy.fixture('rules/_offset0_limit10_total8.json').as('data_rules_offset0_limit10_total8');
-    cy.fixture('rules/_offset5_limit10_total3.json').as('data_rules_offset5_limit10_total3');
+    cy.fixture('rules/get_rules_resp_offset0_limit10_total5.json').as('get_rules_resp_offset0_limit10_total5');
+    cy.fixture('rules/get_rules_resp_offset0_limit10_total8.json').as('get_rules_resp_offset0_limit10_total8');
+    cy.fixture('rules/get_rules_resp_offset5_limit10_total3.json').as('get_rules_resp_offset5_limit10_total3');
   });
 
   context('pagination', () => {
     it('GIVEN total <= pageSize THEN show only one page', () => {
       cy.visit('/rules');
 
-      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@data_rules_offset0_limit10_total5')
-        .as('route_rules_offset0_limit10');
+      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@get_rules_resp_offset0_limit10_total5')
+        .as('get_rules_offset0_limit10');
 
-      cy.wait('@route_rules_offset0_limit10');
+      cy.wait('@get_rules_offset0_limit10');
 
       cy.get('.ant-pagination-next > a').should('have.attr', 'disabled');
     });
@@ -26,10 +26,10 @@ describe('rules_pagination', () => {
     it('GIVEN total > pageSize THEN show two pages', () => {
       cy.visit('/rules');
 
-      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@data_rules_offset0_limit10_total8')
-        .as('route_rules_offset0_limit10');
+      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@get_rules_resp_offset0_limit10_total8')
+        .as('get_rules_offset0_limit10');
 
-      cy.wait('@route_rules_offset0_limit10');
+      cy.wait('@get_rules_offset0_limit10');
 
       cy.get('.ant-pagination-next > a').should('not.have.attr', 'disabled');
     });
@@ -37,17 +37,17 @@ describe('rules_pagination', () => {
     it('GIVEN two pages AND click page 2 THEN load page 2', () => {
       cy.visit('/rules');
 
-      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@data_rules_offset0_limit10_total8')
-        .as('route_rules_offset0_limit10');
+      cy.route('GET', '/api/rules?_offset=0&_limit=10', '@get_rules_resp_offset0_limit10_total8')
+        .as('get_rules_offset0_limit10');
 
-      cy.wait('@route_rules_offset0_limit10');
+      cy.wait('@get_rules_offset0_limit10');
 
-      cy.route('GET', '/api/rules?_offset=5&_limit=10', '@data_rules_offset5_limit10_total3')
-        .as('route_rules_offset5_limit10');
+      cy.route('GET', '/api/rules?_offset=5&_limit=10', '@get_rules_resp_offset5_limit10_total3')
+        .as('get_rules_offset5_limit10');
 
       cy.get('.ant-pagination-next > a').click();
 
-      cy.wait('@route_rules_offset5_limit10');
+      cy.wait('@get_rules_offset5_limit10');
 
       cy.get('.ant-pagination-next > a').should('have.attr', 'disabled');
       cy.get('[data-row-key]').should('have.length', 3);
