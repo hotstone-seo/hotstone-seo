5b7ce2338043fdf7bfc48f004a931683e7ee30f3
Hawari Rahman <hawari.rahman17@gmail.com>
2020-03-20 10:39:20 +0700

    Add Find and FindOne test to tag controller

--- server/controller/tag_cntrl_test.go
+++ server/controller/tag_cntrl_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/hotstone-seo/hotstone-seo/server/controller"
 	"github.com/hotstone-seo/hotstone-seo/server/mock_service"
+	"github.com/hotstone-seo/hotstone-seo/server/repository"
 )
 
 func TestTagController_Create(t *testing.T) {
@@ -33,11 +34,88 @@ func TestTagController_Create(t *testing.T) {
 		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`)
 		require.EqualError(t, err, "code=422, message=insert error")
 	})
-	t.Run("When successfully insert new tag", func(t *testing.T) {
+	t.Run("WHEN successfully insert new tag", func(t *testing.T) {
 		tagSvcMock.EXPECT().Insert(gomock.Any(), gomock.Any()).Return(int64(999), nil)
 		rr, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`)
 		require.NoError(t, err)
 		require.Equal(t, http.StatusCreated, rr.Code)
 		require.Equal(t, "{\"id\":999,\"rule_id\":999,\"locale\":\"en_US\",\"type\":\"title\",\"attributes\":null,\"value\":\"Page Title\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}\n", rr.Body.String())
+	})
+}
+
+func TestTagController_Find(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tagSvcMock := mock_service.NewMockTagService(ctrl)
+	tagCntrl := controller.TagCntrl{
+		TagService: tagSvcMock,
+	}
+	t.Run("WHEN rule_id is not an integer", func(t *testing.T) {
+		_, err := echotest.DoGET(tagCntrl.Find, "/tags?rule_id=invalid", nil)
+		require.EqualError(t, err, "code=400, message=Invalid Rule ID")
+	})
+	t.Run("WHEN received error", func(t *testing.T) {
+		tagSvcMock.EXPECT().Find(gomock.Any(), gomock.Any()).Return(nil, errors.New("retrieve error"))
+		_, err := echotest.DoGET(tagCntrl.Find, "/tags?rule_id=999&locale=en_US", nil)
+		require.EqualError(t, err, "code=500, message=retrieve error")
+	})
+	t.Run("WHEN successful", func(t *testing.T) {
+		tagSvcMock.EXPECT().Find(gomock.Any(), gomock.Any()).Return(
+			[]*repository.Tag{
+				&repository.Tag{
+					ID:         999,
+					RuleID:     999,
+					Locale:     "en_US",
+					Type:       "title",
+					Value:      "Page Title",
+					Attributes: []byte("{}"),
+				},
+			},
+			nil,
+		)
+		rr, err := echotest.DoGET(tagCntrl.Find, "/tags?rule_id=999&locale=en_US", nil)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rr.Code)
+		require.Equal(t, "[{\"id\":999,\"rule_id\":999,\"locale\":\"en_US\",\"type\":\"title\",\"attributes\":{},\"value\":\"Page Title\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}]\n", rr.Body.String())
+	})
+}
+
+func TestTagController_FindOne(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	tagSvcMock := mock_service.NewMockTagService(ctrl)
+	tagCntrl := controller.TagCntrl{
+		TagService: tagSvcMock,
+	}
+	t.Run("WHEN ID param is not an integer", func(t *testing.T) {
+		_, err := echotest.DoGET(tagCntrl.FindOne, "/", map[string]string{"id": "invalid"})
+		require.EqualError(t, err, "code=400, message=Invalid ID")
+	})
+	t.Run("WHEN received error", func(t *testing.T) {
+		tagSvcMock.EXPECT().FindOne(gomock.Any(), gomock.Any()).Return(nil, errors.New("find error"))
+		_, err := echotest.DoGET(tagCntrl.FindOne, "/", map[string]string{"id": "999"})
+		require.EqualError(t, err, "code=500, message=find error")
+	})
+	t.Run("WHEN tag not found", func(t *testing.T) {
+		tagSvcMock.EXPECT().FindOne(gomock.Any(), gomock.Any()).Return(nil, nil)
+		_, err := echotest.DoGET(tagCntrl.FindOne, "/", map[string]string{"id": "999"})
+		require.EqualError(t, err, "code=404, message=Tag#999 not found")
+	})
+	t.Run("WHEN successful", func(t *testing.T) {
+		tagSvcMock.EXPECT().FindOne(gomock.Any(), gomock.Any()).Return(
+			&repository.Tag{
+				ID:         999,
+				RuleID:     999,
+				Locale:     "en_US",
+				Type:       "title",
+				Value:      "Page Title",
+				Attributes: []byte("{}"),
+			},
+			nil,
+		)
+		rr, err := echotest.DoGET(tagCntrl.FindOne, "/", map[string]string{"id": "999"})
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rr.Code)
+		require.Equal(t, "{\"id\":999,\"rule_id\":999,\"locale\":\"en_US\",\"type\":\"title\",\"attributes\":{},\"value\":\"Page Title\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}\n", rr.Body.String())
 	})
 }
