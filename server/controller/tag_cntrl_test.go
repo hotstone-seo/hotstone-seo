675048a643f6fc5014ba40564dd9749624f2f803
Hawari Rahman <hawari.rahman17@gmail.com>
2020-03-23 07:16:05 +0700

    Fix test in tag controller

--- server/controller/tag_cntrl_test.go
+++ server/controller/tag_cntrl_test.go
@@ -22,21 +22,21 @@ func TestTagController_Create(t *testing.T) {
 		TagService: tagSvcMock,
 	}
 	t.Run("WHEN body is malformed", func(t *testing.T) {
-		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id", "type": "title" }`)
+		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id", "type": "title" }`, nil)
 		require.EqualError(t, err, "code=400, message=Syntax error: offset=12, error=invalid character ',' after object key")
 	})
 	t.Run("WHEN tag is invalid", func(t *testing.T) {
-		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title" }`)
+		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title" }`, nil)
 		require.EqualError(t, err, "code=400, message=Key: 'Tag.Value' Error:Field validation for 'Value' failed on the 'noempty' tag")
 	})
 	t.Run("WHEN received error after inserting", func(t *testing.T) {
 		tagSvcMock.EXPECT().Insert(gomock.Any(), gomock.Any()).Return(int64(-1), errors.New("insert error"))
-		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`)
+		_, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`, nil)
 		require.EqualError(t, err, "code=422, message=insert error")
 	})
 	t.Run("WHEN successfully insert new tag", func(t *testing.T) {
 		tagSvcMock.EXPECT().Insert(gomock.Any(), gomock.Any()).Return(int64(999), nil)
-		rr, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`)
+		rr, err := echotest.DoPOST(tagCntrl.Create, "/", `{ "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`, nil)
 		require.NoError(t, err)
 		require.Equal(t, http.StatusCreated, rr.Code)
 		require.Equal(t, "{\"id\":999,\"rule_id\":999,\"locale\":\"en_US\",\"type\":\"title\",\"attributes\":null,\"value\":\"Page Title\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}\n", rr.Body.String())
@@ -153,10 +153,27 @@ func TestTagController_Update(t *testing.T) {
 		TagService: tagSvcMock,
 	}
 	t.Run("WHEN body is malformed", func(t *testing.T) {
-		_, err := echotest.DoPUT(tagCntrl.Update, "/", `{ "rule_id", "type": "title" }`)
+		_, err := echotest.DoPUT(tagCntrl.Update, "/", `{ "rule_id", "type": "title" }`, nil)
 		require.EqualError(t, err, "code=400, message=Syntax error: offset=12, error=invalid character ',' after object key")
 	})
-	t.Run("WHEN id is not an integer", func(t *testing.T) {
-		// TODO: echotest DoPUT should allow passing context param
+	t.Run("WHEN id is invalid", func(t *testing.T) {
+		_, err := echotest.DoPUT(tagCntrl.Update, "/", `{ "id": -1, "rule_id": 999, "type": "title" }`, nil)
+		require.EqualError(t, err, "code=400, message=Invalid ID")
+	})
+	t.Run("WHEN tag is invalid", func(t *testing.T) {
+		_, err := echotest.DoPUT(tagCntrl.Update, "/", `{ "id": 999, "rule_id": 999, "locale": "en_US", "type": "title" }`, nil)
+		require.EqualError(t, err, "code=400, message=Key: 'Tag.Value' Error:Field validation for 'Value' failed on the 'noempty' tag")
+	})
+	t.Run("WHEN received error after inserting", func(t *testing.T) {
+		tagSvcMock.EXPECT().Update(gomock.Any(), gomock.Any()).Return(errors.New("update error"))
+		_, err := echotest.DoPUT(tagCntrl.Update, "/", `{ "id": 999, "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`, nil)
+		require.EqualError(t, err, "code=500, message=update error")
+	})
+	t.Run("WHEN successfully insert new tag", func(t *testing.T) {
+		tagSvcMock.EXPECT().Update(gomock.Any(), gomock.Any()).Return(nil)
+		rr, err := echotest.DoPUT(tagCntrl.Update, "/", `{ "id": 999, "rule_id": 999, "locale": "en_US", "type": "title", "value": "Page Title" }`, nil)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rr.Code)
+		require.Equal(t, "{\"message\":\"Success update tag #999\"}\n", rr.Body.String())
 	})
 }
