97011269ad64dbe4379a7bf110b0ac2f6a148e59
hendri <hendri.chia@tiket.com>
2020-03-24 13:08:01 +0700

    migration: softdelete , start & stop  rule

--- scripts/db/migration/1_rules.up.sql
+++ scripts/db/migration/1_rules.up.sql
@@ -1,9 +1,14 @@
+CREATE TYPE enum_status AS ENUM ('start', 'stop', '') ; 
+
 CREATE TABLE rules (
     id serial PRIMARY KEY,
     data_source_id INTEGER,
     "name" VARCHAR (255) NOT NULL,
     url_pattern TEXT NOT NULL UNIQUE,
     is_active CHAR(1) DEFAULT '1',
+    status enum_status DEFAULT '',
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP DEFAULT NULL,
+    change_status_at TIMESTAMP DEFAULT NULL
 );
