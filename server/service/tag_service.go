66f7c4e9ecbde6f18c574a06a58214e6b6ddfd76
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-03-29 08:39:59 +0700

    TagService: add audit trail

--- server/service/tag_service.go
+++ server/service/tag_service.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"context"
+
 	"github.com/hotstone-seo/hotstone-seo/server/repository"
 	"go.uber.org/dig"
 )
@@ -14,9 +16,73 @@ type TagService interface {
 type TagServiceImpl struct {
 	dig.In
 	repository.TagRepo
+	repository.Transactional
+	AuditTrailService
 }
 
 // NewTagService return new instance of TagService [constructor]
 func NewTagService(impl TagServiceImpl) TagService {
 	return &impl
+}
+
+// Insert tag
+func (s *TagServiceImpl) Insert(ctx context.Context, data repository.Tag) (newID int64, err error) {
+	defer s.CommitMe(&ctx)()
+	if newID, err = s.TagRepo.Insert(ctx, data); err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	newData, err := s.TagRepo.FindOne(ctx, newID)
+	if err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	if _, err = s.AuditTrailService.RecordChanges(ctx, "tags", newID, repository.Insert, nil, newData); err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	return newID, nil
+}
+
+// Update tag
+func (s *TagServiceImpl) Update(ctx context.Context, data repository.Tag) (err error) {
+	defer s.CommitMe(&ctx)()
+	oldData, err := s.TagRepo.FindOne(ctx, data.ID)
+	if err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	if err = s.TagRepo.Update(ctx, data); err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	newData, err := s.TagRepo.FindOne(ctx, data.ID)
+	if err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	if _, err = s.AuditTrailService.RecordChanges(ctx, "tags", data.ID, repository.Update, oldData, newData); err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	return nil
+}
+
+// Delete tag
+func (s *TagServiceImpl) Delete(ctx context.Context, id int64) (err error) {
+	defer s.CommitMe(&ctx)()
+	oldData, err := s.TagRepo.FindOne(ctx, id)
+	if err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	if err = s.TagRepo.Delete(ctx, id); err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	if _, err = s.AuditTrailService.RecordChanges(ctx, "tags", id, repository.Delete, oldData, nil); err != nil {
+		s.CancelMe(ctx, err)
+		return
+	}
+	return nil
 }
