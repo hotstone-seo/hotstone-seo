93033f3087dc45405c2c3849c93d67bcd5cd4d12
hendri <hendri.chia@tiket.com>
2020-03-29 16:25:55 +0700

    add data source unit test : findOne

--- server/controller/data_source_cntrl_test.go
+++ server/controller/data_source_cntrl_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/hotstone-seo/hotstone-seo/server/controller"
 	"github.com/hotstone-seo/hotstone-seo/server/mock_service"
+	"github.com/hotstone-seo/hotstone-seo/server/repository"
 	"github.com/stretchr/testify/require"
 	"github.com/typical-go/typical-rest-server/pkg/echotest"
 )
@@ -34,5 +35,72 @@ func TestDataSourceController_Create(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, http.StatusCreated, rr.Code)
 		require.Equal(t, "{\"id\":100,\"name\":\"data-source-name\",\"url\":\"http://data-source-url\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}\n", rr.Body.String())
+	})
+}
+
+func TestDataSourceController_Find(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	dataSourceSvcMock := mock_service.NewMockDataSourceService(ctrl)
+	dataSourceCntrl := controller.DataSourceCntrl{
+		DataSourceService: dataSourceSvcMock,
+	}
+	t.Run("WHEN retrieved error", func(t *testing.T) {
+		dataSourceSvcMock.EXPECT().Find(gomock.Any()).Return(nil, errors.New("retrieve error"))
+		_, err := echotest.DoGET(dataSourceCntrl.Find, "/", nil)
+		require.EqualError(t, err, "code=500, message=retrieve error")
+	})
+	t.Run("WHEN successful", func(t *testing.T) {
+		dataSourceSvcMock.EXPECT().Find(gomock.Any()).Return(
+			[]*repository.DataSource{
+				&repository.DataSource{
+					ID:   100,
+					Name: "Airport Data Source",
+					Url:  "dssource1.com",
+				},
+			},
+			nil,
+		)
+		rr, err := echotest.DoGET(dataSourceCntrl.Find, "/", nil)
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rr.Code)
+		require.Equal(t, "[{\"id\":100,\"name\":\"Airport Data Source\",\"url\":\"dssource1.com\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}]\n", rr.Body.String())
+	})
+}
+
+func TestDataSourceController_FindOne(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	dataSourceSvcMock := mock_service.NewMockDataSourceService(ctrl)
+	dataSourceCntrl := controller.DataSourceCntrl{
+		DataSourceService: dataSourceSvcMock,
+	}
+	t.Run("WHEN id is not an integer", func(t *testing.T) {
+		_, err := echotest.DoGET(dataSourceCntrl.FindOne, "/", map[string]string{"id": "invalid"})
+		require.EqualError(t, err, "code=400, message=Invalid ID")
+	})
+	t.Run("WHEN received error", func(t *testing.T) {
+		dataSourceSvcMock.EXPECT().FindOne(gomock.Any(), int64(100)).Return(nil, errors.New("find error"))
+		_, err := echotest.DoGET(dataSourceCntrl.FindOne, "/", map[string]string{"id": "100"})
+		require.EqualError(t, err, "code=500, message=find error")
+	})
+	t.Run("WHEN Data Source is not found", func(t *testing.T) {
+		dataSourceSvcMock.EXPECT().FindOne(gomock.Any(), int64(100)).Return(nil, nil)
+		_, err := echotest.DoGET(dataSourceCntrl.FindOne, "/", map[string]string{"id": "100"})
+		require.EqualError(t, err, "code=404, message=DataSource#100 not found")
+	})
+	t.Run("WHEN successful", func(t *testing.T) {
+		dataSourceSvcMock.EXPECT().FindOne(gomock.Any(), int64(100)).Return(
+			&repository.DataSource{
+				ID:   100,
+				Name: "Airport Data Source",
+				Url:  "dssource1.com",
+			},
+			nil,
+		)
+		rr, err := echotest.DoGET(dataSourceCntrl.FindOne, "/", map[string]string{"id": "100"})
+		require.NoError(t, err)
+		require.Equal(t, http.StatusOK, rr.Code)
+		require.Equal(t, "{\"id\":100,\"name\":\"Airport Data Source\",\"url\":\"dssource1.com\",\"updated_at\":\"0001-01-01T00:00:00Z\",\"created_at\":\"0001-01-01T00:00:00Z\"}\n", rr.Body.String())
 	})
 }
