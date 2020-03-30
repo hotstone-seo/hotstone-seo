6bf4abb97e4a6727f6893384b380c759df7cc98d
Hawari Rahman <hawari.rahman17@gmail.com>
2020-03-30 17:01:24 +0700

    Set all tag types form to be standalone

--- ui/src/components/Tag/CanonicalForm.js
+++ ui/src/components/Tag/CanonicalForm.js
@@ -1,31 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Form, Input } from 'antd';
+import { Form, Input, Select } from 'antd';
+import locales from 'locales';
 
-function CanonicalForm({ form }) {
-  form.setFieldsValue({
-    attributes: { rel: 'canonical' },
-  });
+const { Option } = Select;
+
+function CanonicalForm({ form, tag, onSubmit }) {
+  const { id, rule_id, locale, attributes: { href } } = tag;
+
+  const onFinish = (values) => {
+    const formTag = Object.assign(values, { id, rule_id });
+    onSubmit(formTag);
+  };
+
+  form.setFieldsValue({ locale, href });
 
   return (
-    <>
-      <Form.Item name={['attributes', 'rel']} noStyle />
-
+    <Form
+      form={form}
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 14 }}
+      onFinish={onFinish}
+    >
       <Form.Item
-        label="URL"
-        name={['attributes', 'href']}
+        label="Locale"
+        name="locale"
+        rules={[{ required: true, message: 'Please set a locale for the tag' }]}
+      >
+        <Select
+          data-testid="select-locale"
+          showSearch
+          filterOption={(input, option) => (
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          )}
+        >
+          {locales.map((loc) => (
+            <Option key={loc} value={loc}>{loc}</Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Form.Item
+        label="Href"
+        name="href"
         rules={[{ required: true, message: 'Must provide canonical URL' }]}
       >
         <Input data-testid="input-url" />
       </Form.Item>
-    </>
+    </Form>
   );
 }
+
+CanonicalForm.defaultProps = {
+  form: Form.useForm(),
+  tag: {},
+};
 
 CanonicalForm.propTypes = {
   form: PropTypes.shape({
     setFieldsValue: PropTypes.func.isRequired,
-  }).isRequired,
+  }),
+  onSubmit: PropTypes.func.isRequired,
+  tag: PropTypes.shape({
+    id: PropTypes.number,
+    rule_id: PropTypes.number,
+    type: PropTypes.string,
+    locale: PropTypes.string,
+    attributes: PropTypes.shape({
+      href: PropTypes.string,
+    }),
+  }),
 };
 
 export default CanonicalForm;
--- ui/src/components/Tag/ScriptForm.js
+++ ui/src/components/Tag/ScriptForm.js
@@ -1,18 +1,75 @@
 import React from 'react';
-import { Form, Input } from 'antd';
+import PropTypes from 'prop-types';
+import { Form, Input, Select } from 'antd';
+import locales from 'locales';
 
-function ScriptForm() {
+const { Option } = Select;
+
+function ScriptForm({ form, tag, onSubmit }) {
+  const { id, rule_id, locale, attributes: { src } } = tag;
+
+  const onFinish = (values) => {
+    const formTag = Object.assign(values, { id, rule_id });
+    onSubmit(formTag);
+  };
+
+  form.setFieldsValue({ locale, source: src });
+
   return (
-    <>
+    <Form
+      form={form}
+      labelCol={{ span: 6 }}
+      wrapperCol={{ span: 14 }}
+      onFinish={onFinish}
+    >
+      <Form.Item
+        label="Locale"
+        name="locale"
+        rules={[{ required: true, message: 'Please set a locale for the tag' }]}
+      >
+        <Select
+          data-testid="select-locale"
+          showSearch
+          filterOption={(input, option) => (
+            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+          )}
+        >
+          {locales.map((loc) => (
+            <Option key={loc} value={loc}>{loc}</Option>
+          ))}
+        </Select>
+      </Form.Item>
       <Form.Item
         label="Source"
-        name={['attributes', 'src']}
+        name="source"
         rules={[{ required: true, message: 'Must provide script URL' }]}
       >
         <Input data-testid="input-src" />
       </Form.Item>
-    </>
+    </Form>
   );
 }
+
+ScriptForm.defaultProps = {
+  form: Form.useForm(),
+  tag: {},
+};
+
+ScriptForm.propTypes = {
+  form: PropTypes.shape({
+    setFieldsValue: PropTypes.func,
+  }),
+  onSubmit: PropTypes.func.isRequired,
+  tag: PropTypes.shape({
+    id: PropTypes.number,
+    rule_id: PropTypes.number,
+    type: PropTypes.string,
+    locale: PropTypes.string,
+    attributes: PropTypes.shape({
+      src: PropTypes.string,
+    }),
+  }),
+};
+
 
 export default ScriptForm;
