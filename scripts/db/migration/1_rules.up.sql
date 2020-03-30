db7dbf2f6eee5b3176f61c3c757f87bbb3806e7d
hendri <hendri.chia@tiket.com>
2020-03-30 14:58:45 +0700

    DB Script : remove column is_active

--- scripts/db/migration/1_rules.up.sql
+++ scripts/db/migration/1_rules.up.sql
@@ -5,7 +5,6 @@ CREATE TABLE rules (
     data_source_id INTEGER,
     "name" VARCHAR (255) NOT NULL,
     url_pattern TEXT NOT NULL UNIQUE,
-    is_active CHAR(1) DEFAULT '1',
     status enum_status DEFAULT '',
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
--- scripts/db/migration/4_data_source.up.sql
+++ scripts/db/migration/4_data_source.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE data_sources (
     id serial PRIMARY KEY,
     "name" VARCHAR (255) NOT NULL,
     "url" VARCHAR (255) NOT NULL,
-    is_active CHAR(1) DEFAULT '1',
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMP DEFAULT NULL
