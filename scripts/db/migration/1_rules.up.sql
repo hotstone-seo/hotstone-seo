d6dddf16347725cf8629e20d358943090ffccfb8
hendri <hendri.chia@tiket.com>
2020-03-30 16:21:02 +0700

    DB script : change default value

--- scripts/db/migration/1_rules.up.sql
+++ scripts/db/migration/1_rules.up.sql
@@ -9,6 +9,6 @@ CREATE TABLE rules (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMPTZ DEFAULT NULL,
-    change_status_at TIMESTAMPTZ DEFAULT NULL
+    change_status_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 create index rules_status ON rules(status);
