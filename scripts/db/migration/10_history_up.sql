6da458f8cc6f893e2d71ff750f6fc62459403755
hendri <hendri.chia@tiket.com>
2020-04-01 04:40:18 +0700

    DB script : change field name

--- scripts/db/migration/10_history_up.sql
+++ scripts/db/migration/10_history_up.sql
@@ -2,7 +2,7 @@ CREATE TABLE history (
     id bigserial PRIMARY KEY,
     time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     entity_id BIGINT NOT NULL,
-    "from" VARCHAR (25) NOT NULL,
+    entity_from VARCHAR (25) NOT NULL,
     username VARCHAR (100) NOT NULL,
     data JSONB NOT NULL DEFAULT '{}' :: JSONB
 );
