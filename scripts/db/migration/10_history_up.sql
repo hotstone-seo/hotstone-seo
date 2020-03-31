5ca295a6c2564017b69f34be5d3afcd7b79cc307
hendri <hendri.chia@tiket.com>
2020-03-31 11:28:51 +0700

    script db : add migration create table history for archive data

---
+++ scripts/db/migration/10_history_up.sql
@@ -0,0 +1,8 @@
+CREATE TABLE history (
+    id bigserial PRIMARY KEY,
+    time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    entity_id BIGINT NOT NULL,
+    "from" VARCHAR (25) NOT NULL,
+    username VARCHAR (100) NOT NULL,
+    data JSONB NOT NULL DEFAULT '{}' :: JSONB
+);
