26abe3f745666c83d5c1e1802f8ddda766c00bad
iman tung <iman.tung@gmail.com>
2020-04-01 13:55:00 +0700

    revison: fix datasource seed to use handlebar format

--- scripts/db/seed/data_source.sql
+++ scripts/db/seed/data_source.sql
@@ -1,5 +1,5 @@
 INSERT INTO data_sources (id, "name", "url")
 VALUES 
-    (1, 'events', 'http://localhost:3021/events?name=<eventName>');
+    (1, 'events', 'http://localhost:3021/events?name={{eventName}}');
 
 SELECT setval('data_sources_id_seq', (SELECT MAX(id) FROM data_sources));
