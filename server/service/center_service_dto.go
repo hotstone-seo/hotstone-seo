3339d3fd8f1623edbb50fb24a91646b4b8bf9bd3
Hawari Rahman <hawari.rahman17@gmail.com>
2020-04-03 08:07:21 +0700

    Add ID field in center's Tag DTO

--- server/service/center_service_dto.go
+++ server/service/center_service_dto.go
@@ -1,47 +1,35 @@
 package service
 
-// AddMetaTagRequest is request model for addMetaTag method
-// TODO: remove "Add" from each of the struct name
-type AddMetaTagRequest struct {
+// MetaTagRequest is request model for MetaTag related method
+type MetaTagRequest struct {
+	ID      int64  `json:"id"`
 	RuleID  int64  `json:"ruleID"`
 	Locale  string `json:"locale"`
 	Name    string `json:"name"`
 	Content string `json:"content"`
 }
 
-// AddTitleTagRequest is request model for addTitleTag method
-type AddTitleTagRequest struct {
+// TitleTagRequest is request model for TitleTag related method
+type TitleTagRequest struct {
+	ID     int64  `json:"id"`
 	RuleID int64  `json:"ruleID"`
 	Locale string `json:"locale"`
 	Title  string `json:"title"`
 }
 
-// AddCanonicalTagRequest is request model for addCanonicalTag method
-type AddCanonicalTagRequest struct {
+// CanonicalTagRequest is request model for CanonicalTag related method
+type CanonicalTagRequest struct {
+	ID     int64  `json:"id"`
 	RuleID int64  `json:"ruleID"`
 	Locale string `json:"locale"`
 	Href   string `json:"href"`
 }
 
-// AddScriptTagRequest is request model for addScriptTag method
-type AddScriptTagRequest struct {
+// ScriptTagRequest is request model for ScriptTag related method
+type ScriptTagRequest struct {
+	ID     int64  `json:"id"`
 	Type   string `json:"type"`
 	RuleID int64  `json:"ruleID"`
 	Locale string `json:"locale"`
 	Source string `json:"source"`
-}
-
-func NewTagRequest(tagType string) interface{} {
-	switch tagType {
-	case "meta":
-		return &AddMetaTagRequest{}
-	case "title":
-		return &AddTitleTagRequest{}
-	case "canonical":
-		return &AddCanonicalTagRequest{}
-	case "script":
-		return &AddScriptTagRequest{}
-	default:
-		return nil
-	}
 }
