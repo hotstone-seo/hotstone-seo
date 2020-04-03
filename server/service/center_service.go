039019dd1d47d9a0da6a405f0805ef0f97cb41e0
Hawari Rahman <hawari.rahman17@gmail.com>
2020-04-03 15:13:42 +0700

    Add update methods to /center

--- server/service/center_service.go
+++ server/service/center_service.go
@@ -13,9 +13,13 @@ import (
 // CenterService is center related logic [mock]
 type CenterService interface {
 	AddMetaTag(ctx context.Context, req MetaTagRequest) (*repository.Tag, error)
+	UpdateMetaTag(ctx context.Context, req MetaTagRequest) error
 	AddTitleTag(ctx context.Context, req TitleTagRequest) (*repository.Tag, error)
+	UpdateTitleTag(ctx context.Context, req TitleTagRequest) error
 	AddCanonicalTag(ctx context.Context, req CanonicalTagRequest) (*repository.Tag, error)
+	UpdateCanonicalTag(ctx context.Context, req CanonicalTagRequest) error
 	AddScriptTag(ctx context.Context, req ScriptTagRequest) (*repository.Tag, error)
+	UpdateScriptTag(ctx context.Context, req ScriptTagRequest) error
 }
 
 // CenterServiceImpl implementation of CenterService
@@ -29,7 +33,8 @@ func NewCenterService(impl CenterServiceImpl) CenterService {
 	return &impl
 }
 
-// AddMetaTag to add metaTag
+// AddMetaTag adds new meta tag
+// TODO: Use JSON marshal to set attributes, simple string substitution is prone to be exploited
 func (i *CenterServiceImpl) AddMetaTag(ctx context.Context, req MetaTagRequest) (tag *repository.Tag, err error) {
 	tag = &repository.Tag{
 		RuleID:     req.RuleID,
@@ -44,7 +49,20 @@ func (i *CenterServiceImpl) AddMetaTag(ctx context.Context, req MetaTagRequest) 
 	return
 }
 
-// AddTitleTag to add titleTag
+// UpdateMetaTag updates existing meta tag
+func (i *CenterServiceImpl) UpdateMetaTag(ctx context.Context, req MetaTagRequest) error {
+	return i.TagService.Update(ctx, repository.Tag{
+		ID:         req.ID,
+		RuleID:     req.RuleID,
+		Locale:     req.Locale,
+		Type:       "meta",
+		Attributes: dbtype.JSON(fmt.Sprintf(`{"name":"%s", "content":"%s"}`, req.Name, req.Content)),
+		Value:      "",
+		UpdatedAt:  time.Now(),
+	})
+}
+
+// AddTitleTag adds new title tag
 func (i *CenterServiceImpl) AddTitleTag(ctx context.Context, req TitleTagRequest) (tag *repository.Tag, err error) {
 	tag = &repository.Tag{
 		RuleID:     req.RuleID,
@@ -59,7 +77,20 @@ func (i *CenterServiceImpl) AddTitleTag(ctx context.Context, req TitleTagRequest
 	return
 }
 
-// AddCanonicalTag to add canonicalTag
+// UpdateTitleTag updates existing title tag
+func (i *CenterServiceImpl) UpdateTitleTag(ctx context.Context, req TitleTagRequest) error {
+	return i.TagService.Update(ctx, repository.Tag{
+		ID:         req.ID,
+		RuleID:     req.RuleID,
+		Locale:     req.Locale,
+		Type:       "title",
+		Attributes: dbtype.JSON(`{}`),
+		Value:      req.Title,
+		UpdatedAt:  time.Now(),
+	})
+}
+
+// AddCanonicalTag adds new canonical tag
 func (i *CenterServiceImpl) AddCanonicalTag(ctx context.Context, req CanonicalTagRequest) (tag *repository.Tag, err error) {
 	tag = &repository.Tag{
 		RuleID:     req.RuleID,
@@ -74,7 +105,20 @@ func (i *CenterServiceImpl) AddCanonicalTag(ctx context.Context, req CanonicalTa
 	return
 }
 
-// AddScriptTag to add scriptTag
+// UpdateCanonicalTag updates existing canonical tag
+func (i *CenterServiceImpl) UpdateCanonicalTag(ctx context.Context, req CanonicalTagRequest) error {
+	return i.TagService.Update(ctx, repository.Tag{
+		ID:         req.ID,
+		RuleID:     req.RuleID,
+		Locale:     req.Locale,
+		Type:       "link",
+		Attributes: dbtype.JSON(fmt.Sprintf(`{"href":"%s","rel":"canonical"}`, req.Href)),
+		Value:      "",
+		UpdatedAt:  time.Now(),
+	})
+}
+
+// AddScriptTag adds new script tag
 func (i *CenterServiceImpl) AddScriptTag(ctx context.Context, req ScriptTagRequest) (tag *repository.Tag, err error) {
 	tag = &repository.Tag{
 		RuleID:     req.RuleID,
@@ -87,4 +131,17 @@ func (i *CenterServiceImpl) AddScriptTag(ctx context.Context, req ScriptTagReque
 	}
 	tag.ID, err = i.TagService.Insert(ctx, *tag)
 	return
+}
+
+// UpdateScriptTag updates existing script tag
+func (i *CenterServiceImpl) UpdateScriptTag(ctx context.Context, req ScriptTagRequest) error {
+	return i.TagService.Update(ctx, repository.Tag{
+		ID:         req.ID,
+		RuleID:     req.RuleID,
+		Locale:     req.Locale,
+		Type:       "script",
+		Attributes: dbtype.JSON(fmt.Sprintf(`{"source":"%s"}`, req.Source)),
+		Value:      req.Type,
+		UpdatedAt:  time.Now(),
+	})
 }
