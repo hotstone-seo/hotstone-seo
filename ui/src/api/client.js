6ea9fad9f51cc118202b4bb9eebf2435ed4f395b
hendri <hendri.chia@tiket.com>
2020-04-07 05:38:56 +0700

    ui : handle response 504

--- ui/src/api/client.js
+++ ui/src/api/client.js
@@ -9,22 +9,22 @@ client.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response) {
-      if (error.response.status === 401) {
-        message.error('Session expired. Please login first...');
-        window.location.href = '/login';
-      } else if (error.response.status === 504) {
-        message.error('Network error.Failed to connect API');
-        // return Promise.reject('Network error.Failed to connect API');
-      }
       /* const {
         data: { message },
       } = error.response;
       return Promise.reject(
         new Error(message || 'Unexpected error occured in server'),
       ); */
+      if (error.response.status === 504) {
+        message.error('Network error.Failed to connect API');
+        throw error; // return Promise.reject(error || 'Network error.Failed to connect API');
+      } else if (error.response.status === 401) {
+        message.error('Session expired. Please login first...');
+        window.location.href = '/login';
+      }
     }
-    return error;
-    // return Promise.reject(error.message);
+    // return Promise.reject(error);
+    throw error;
   },
 );
 
