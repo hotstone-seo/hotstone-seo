b51c5d40e4f56d90c076b2f434379e6da4353e52
Hawari Rahman <hawari.rahman17@gmail.com>
2020-04-06 17:21:45 +0700

    Add missing mock methods from CenterService

--- server/mock_service/center_service.go
+++ server/mock_service/center_service.go
@@ -94,3 +94,59 @@ func (mr *MockCenterServiceMockRecorder) AddTitleTag(arg0, arg1 interface{}) *go
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "AddTitleTag", reflect.TypeOf((*MockCenterService)(nil).AddTitleTag), arg0, arg1)
 }
+
+// UpdateCanonicalTag mocks base method
+func (m *MockCenterService) UpdateCanonicalTag(arg0 context.Context, arg1 service.CanonicalTagRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateCanonicalTag", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateCanonicalTag indicates an expected call of UpdateCanonicalTag
+func (mr *MockCenterServiceMockRecorder) UpdateCanonicalTag(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateCanonicalTag", reflect.TypeOf((*MockCenterService)(nil).UpdateCanonicalTag), arg0, arg1)
+}
+
+// UpdateMetaTag mocks base method
+func (m *MockCenterService) UpdateMetaTag(arg0 context.Context, arg1 service.MetaTagRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateMetaTag", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateMetaTag indicates an expected call of UpdateMetaTag
+func (mr *MockCenterServiceMockRecorder) UpdateMetaTag(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateMetaTag", reflect.TypeOf((*MockCenterService)(nil).UpdateMetaTag), arg0, arg1)
+}
+
+// UpdateScriptTag mocks base method
+func (m *MockCenterService) UpdateScriptTag(arg0 context.Context, arg1 service.ScriptTagRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateScriptTag", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateScriptTag indicates an expected call of UpdateScriptTag
+func (mr *MockCenterServiceMockRecorder) UpdateScriptTag(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateScriptTag", reflect.TypeOf((*MockCenterService)(nil).UpdateScriptTag), arg0, arg1)
+}
+
+// UpdateTitleTag mocks base method
+func (m *MockCenterService) UpdateTitleTag(arg0 context.Context, arg1 service.TitleTagRequest) error {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "UpdateTitleTag", arg0, arg1)
+	ret0, _ := ret[0].(error)
+	return ret0
+}
+
+// UpdateTitleTag indicates an expected call of UpdateTitleTag
+func (mr *MockCenterServiceMockRecorder) UpdateTitleTag(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "UpdateTitleTag", reflect.TypeOf((*MockCenterService)(nil).UpdateTitleTag), arg0, arg1)
+}
