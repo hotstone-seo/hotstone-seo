d334fca42de829f572b23c259c772236aa3e900b
hendri <hendri.chia@tiket.com>
2020-04-08 06:44:26 +0700

    handle 504 : uncomment code because does not result in a browser hang

--- ui/src/views/MismatchRule/scenes/ViewMismatchRules.js
+++ ui/src/views/MismatchRule/scenes/ViewMismatchRules.js
@@ -72,7 +72,7 @@ function ViewMismatchRules(props) {
           setNextPageToken(nextPageToken);
           setListData(listData);
         } else {
-          // previousPage();
+          previousPage();
         }
       } catch (err) {
         console.log('ERR: ', err);
