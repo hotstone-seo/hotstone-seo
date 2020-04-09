fbf55aacdd3c9412a11675f66925bd9d6d93c7ac
Hawari Rahman <hawari.rahman17@gmail.com>
2020-04-09 08:28:44 +0700

    Add TagPreview re-rendering mechanism in TitleForm

--- ui/src/components/Tag/TitleForm.js
+++ ui/src/components/Tag/TitleForm.js
@@ -1,58 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Input, Select, Button } from 'antd';
+import {
+  Form, Input, Select, Button,
+} from 'antd';
 import locales from 'locales';
+import TagPreview from './TagPreview';
 
 const { Option } = Select;
 
 function TitleForm({ tag, onSubmit }) {
   const [form] = Form.useForm();
-  const { id, rule_id, locale, value } = tag;
+  const [tagPreview, setTagPreview] = useState(Object.assign(tag, { type: 'title' }));
+  const {
+    id, rule_id, locale, value,
+  } = tag;
 
   const onFinish = (values) => {
     const formTag = Object.assign(values, { id, rule_id });
     onSubmit(formTag);
   };
 
-  form.setFieldsValue({ locale, title: value });
+  const updateTagPreview = ({ title }) => {
+    if (title) {
+      setTagPreview(Object.assign(tagPreview, { value: title }));
+    }
+  };
 
   return (
-    <Form
-      form={form}
-      labelCol={{ span: 6 }}
-      wrapperCol={{ span: 14 }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        label="Locale"
-        name="locale"
-        rules={[{ required: true, message: 'Please set a locale for the tag' }]}
+    <>
+      <Form
+        form={form}
+        labelCol={{ span: 6 }}
+        wrapperCol={{ span: 14 }}
+        initialValues={{ locale, title: value }}
+        onFinish={onFinish}
+        onValuesChange={updateTagPreview}
       >
-        <Select
-          data-testid="select-locale"
-          showSearch
-          filterOption={(input, option) => (
-            option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-          )}
+        <Form.Item
+          label="Locale"
+          name="locale"
+          rules={[{ required: true, message: 'Please set a locale for the tag' }]}
         >
-          {locales.map((loc) => (
-            <Option key={loc} value={loc}>{loc}</Option>
-          ))}
-        </Select>
-      </Form.Item>
-      <Form.Item
-        label="Title"
-        name="title"
-        rules={[{ required: true, message: 'Must provide a title' }]}
-      >
-        <Input data-testid="input-title" />
-      </Form.Item>
-      <Form.Item wrapperCol={{ offset: 6, span: 14 }}>
-        <Button data-testid="btn-save" type="primary" htmlType="submit">
-          Save
-        </Button>
-      </Form.Item>
-    </Form>
+          <Select
+            data-testid="select-locale"
+            showSearch
+            filterOption={(input, option) => (
+              option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+            )}
+          >
+            {locales.map((loc) => (
+              <Option key={loc} value={loc}>{loc}</Option>
+            ))}
+          </Select>
+        </Form.Item>
+        <Form.Item
+          label="Title"
+          name="title"
+          rules={[{ required: true, message: 'Must provide a title' }]}
+        >
+          <Input data-testid="input-title" />
+        </Form.Item>
+        <Form.Item wrapperCol={{ offset: 6, span: 14 }}>
+          <Button data-testid="btn-save" type="primary" htmlType="submit">
+            Save
+          </Button>
+        </Form.Item>
+      </Form>
+      <TagPreview tag={tagPreview} />
+    </>
   );
 }
 
