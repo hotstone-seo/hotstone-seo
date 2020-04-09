51ea8c0b7b1634745b6aa29bb669f3bfd6d3c3d8
Hawari Rahman <hawari.rahman17@gmail.com>
2020-04-09 08:47:39 +0700

    Set type in TagPreview to optional

--- ui/src/components/Tag/TagPreview.js
+++ ui/src/components/Tag/TagPreview.js
@@ -4,20 +4,21 @@ import { renderToStaticMarkup } from 'react-dom/server';
 
 function TagPreview({ tag }) {
   const { type, attributes, value } = tag;
-  return (type
-    ? (
-      <pre>
-        {renderToStaticMarkup(
-          React.createElement(type, attributes, value === '' ? null : value),
-        )}
-      </pre>
-    ) : null
+  if (!type) {
+    return null;
+  }
+  return (
+    <pre>
+      {renderToStaticMarkup(
+        React.createElement(type, attributes, value === '' ? null : value),
+      )}
+    </pre>
   );
 }
 
 TagPreview.propTypes = {
   tag: PropTypes.shape({
-    type: PropTypes.string.isRequired,
+    type: PropTypes.string,
     attributes: PropTypes.object,
     value: PropTypes.string,
   }).isRequired,
