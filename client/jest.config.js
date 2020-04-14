dd495739d5dcaae9309cdfc9250040dd43a1d844
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-04-14 09:13:08 +0700

    hotstone-client: setup jest

---
+++ client/jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    verbose: true,
+    roots: ["src/"],
+    transform: {
+        "^.+\\.jsx?$": "babel-jest"
+    },
+};
---
+++ client/src/index.test.js
@@ -0,0 +1,6 @@
+import { HotStoneClient } from './index'
+
+test('adds 1 + 2 to equal 3', () => {
+    console.log("HotStoneClient: ", HotStoneClient)
+    expect(3).toBe(3);
+});
