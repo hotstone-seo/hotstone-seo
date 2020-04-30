69ee557cea1b3b0dc381555c35c671013e3a1bfc
Hawari Rahman <hawari.rahman17@gmail.com>
2020-04-30 08:37:26 +0700

    Fix tag service rollback bug

--- server/service/tag_service.go
+++ server/service/tag_service.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/hotstone-seo/hotstone-seo/pkg/dbtxn"
 	"github.com/hotstone-seo/hotstone-seo/server/repository"
+	log "github.com/sirupsen/logrus"
 	"go.uber.org/dig"
 )
 
@@ -29,66 +30,77 @@ func NewTagService(impl TagServiceImpl) TagService {
 
 // Insert tag
 func (s *TagServiceImpl) Insert(ctx context.Context, data repository.Tag) (newID int64, err error) {
-	defer s.CommitMe(&ctx)()
-	if newID, err = s.TagRepo.Insert(ctx, data); err != nil {
-		s.CancelMe(ctx, err)
+	if data.ID, err = s.TagRepo.Insert(ctx, data); err != nil {
 		return
 	}
-	newData, err := s.TagRepo.FindOne(ctx, newID)
-	if err != nil {
-		s.CancelMe(ctx, err)
-		return
-	}
-	if _, err = s.AuditTrailService.RecordChanges(ctx, "tags", newID, repository.Insert, nil, newData); err != nil {
-		s.CancelMe(ctx, err)
-		return
-	}
-	return newID, nil
+	go func() {
+		if _, auditErr := s.AuditTrailService.RecordChanges(
+			ctx,
+			"tags",
+			data.ID,
+			repository.Insert,
+			nil,
+			data,
+		); auditErr != nil {
+			log.Error(auditErr)
+		}
+	}()
+	return data.ID, nil
 }
 
 // Update tag
 func (s *TagServiceImpl) Update(ctx context.Context, data repository.Tag) (err error) {
-	defer s.CommitMe(&ctx)()
-	oldData, err := s.TagRepo.FindOne(ctx, data.ID)
-	if err != nil {
-		s.CancelMe(ctx, err)
+	var oldData *repository.Tag
+	if oldData, err = s.TagRepo.FindOne(ctx, data.ID); err != nil {
 		return
 	}
 	if err = s.TagRepo.Update(ctx, data); err != nil {
-		s.CancelMe(ctx, err)
 		return
 	}
-	newData, err := s.TagRepo.FindOne(ctx, data.ID)
-	if err != nil {
-		s.CancelMe(ctx, err)
-		return
-	}
-	if _, err = s.AuditTrailService.RecordChanges(ctx, "tags", data.ID, repository.Update, oldData, newData); err != nil {
-		s.CancelMe(ctx, err)
-		return
-	}
+	go func() {
+		if _, auditErr := s.AuditTrailService.RecordChanges(
+			ctx,
+			"tags",
+			data.ID,
+			repository.Update,
+			oldData,
+			data,
+		); auditErr != nil {
+			log.Error(auditErr)
+		}
+	}()
 	return nil
 }
 
 // Delete tag
 func (s *TagServiceImpl) Delete(ctx context.Context, id int64) (err error) {
-	defer s.CommitMe(&ctx)()
-	oldData, err := s.TagRepo.FindOne(ctx, id)
-	if err != nil {
-		s.CancelMe(ctx, err)
-		return
-	}
-	if _, err = s.HistoryService.RecordHistory(ctx, oldData.Type+"-tag", id, oldData); err != nil {
-		s.CancelMe(ctx, err)
+	var oldData *repository.Tag
+	if oldData, err = s.TagRepo.FindOne(ctx, id); err != nil {
 		return
 	}
 	if err = s.TagRepo.Delete(ctx, id); err != nil {
 		s.CancelMe(ctx, err)
 		return
 	}
-	if _, err = s.AuditTrailService.RecordChanges(ctx, "tags", id, repository.Delete, oldData, nil); err != nil {
-		s.CancelMe(ctx, err)
-		return
-	}
+	go func() {
+		if _, histErr := s.HistoryService.RecordHistory(
+			ctx,
+			oldData.Type+"-tag",
+			id,
+			oldData,
+		); histErr != nil {
+			log.Error(histErr)
+		}
+		if _, auditErr := s.AuditTrailService.RecordChanges(
+			ctx,
+			"tags",
+			id,
+			repository.Delete,
+			oldData,
+			nil,
+		); auditErr != nil {
+			log.Error(auditErr)
+		}
+	}()
 	return nil
 }
