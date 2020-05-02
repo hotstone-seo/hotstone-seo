8ad95edc3f801f95d4db3e240c35398db737964f
hendri <hendri.chia@tiket.com>
2020-05-03 04:56:48 +0700

    DB migration : change role type user login

--- scripts/db/seed/role_type.sql
+++ scripts/db/seed/role_type.sql
@@ -1,6 +1,6 @@
 INSERT INTO role_type (id, "type")
 VALUES 
-    (1, 'ADMIN'),
-    (2, 'USER');
+    (1, 'SUPER ADMIN'),
+    (2, 'ADMIN');
 
 SELECT setval('role_type_id_seq', (SELECT MAX(id) FROM role_type));
