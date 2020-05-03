6be446c2c7eab19d7d8b2d1c619e614e3a522417
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-05-03 17:42:02 +0700

    links: update page with parameter

--- client/examples/server-side-rendering/src/links.js
+++ client/examples/server-side-rendering/src/links.js
@@ -1,6 +1,6 @@
 const links = [
   { to: '/airports', label: 'Static Page', exact: true },
-  { to: '/airports/12', label: 'Page with Parameter' },
+  { to: '/events/bigbang', label: 'Page with Parameter' },
   { to: '/nonexistent', label: 'Non-existent Page', exact: true }
 ];
 
