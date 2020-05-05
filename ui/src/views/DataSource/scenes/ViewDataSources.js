b569aa1c0e0d0e25dd816a3ef79026c56af7bd76
Hawari Rahman <hawari.rahman17@gmail.com>
2020-05-05 16:15:52 +0700

    Use react hook to reuse fetching routine

--- ui/src/views/DataSource/scenes/ViewDataSources.js
+++ ui/src/views/DataSource/scenes/ViewDataSources.js
@@ -2,15 +2,21 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { PageHeader, Button, message } from 'antd';
-import { deleteDataSource } from 'api/datasource';
-import useDataSources from 'hooks/useDataSources';
+import { fetchDataSources, deleteDataSource } from 'api/datasource';
+import useAsync from 'hooks/useAsync';
 import { DataSourceList } from 'components/DataSource';
 
 import { PlusOutlined } from '@ant-design/icons';
 
 function ViewDataSources({ match }) {
-  const [dataSources, setDataSources] = useDataSources();
   const history = useHistory();
+  const {
+    pending, value: dataSources, setValue: setDataSources, error,
+  } = useAsync(fetchDataSources);
+
+  if (error) {
+    message.error(error.message);
+  }
 
   const showEditScene = (dataSource) => {
     history.push(`${match.url}/${dataSource.id}`);
@@ -20,10 +26,12 @@ function ViewDataSources({ match }) {
     deleteDataSource(dataSource.id)
       .then(() => {
         message.success(`Successfully deleted ${dataSource.name}`);
-        setDataSources(dataSources.filter((item) => item.id !== dataSource.id));
+        setDataSources(
+          dataSources.filter((item) => item.id !== dataSource.id),
+        );
       })
-      .catch((error) => {
-        message.error(error.message);
+      .catch((err) => {
+        message.error(err.message);
       });
   };
 
@@ -49,6 +57,7 @@ function ViewDataSources({ match }) {
         </Button>
         <DataSourceList
           dataSources={dataSources}
+          loading={pending}
           onClick={showEditScene}
           onEdit={showEditScene}
           onDelete={removeDataSource}
