e460d117dbfac3c2a67b080c3e706f0ef3d76581
Hawari Rahman <hawari.rahman17@gmail.com>
2020-05-12 14:25:03 +0700

    Add DTO for holding LocalBusiness request

--- server/service/center_service_dto.go
+++ server/service/center_service_dto.go
@@ -55,3 +55,30 @@ type BreadcrumbItem struct {
 	Name string `json:"name"`
 	Item string `json:"item"`
 }
+
+type LocalBusinessRequest struct {
+	ID              int64           `json:"id"`
+	RuleID          int64           `json:"rule_id"`
+	Name            string          `json:"name"`
+	Image           string          `json:"image"`
+	URL             string          `json:"url"`
+	Phone           string          `json:"phone"`
+	PriceRange      string          `json:"priceRange"`
+	Address         Address         `json:"address"`
+	AggregateRating AggregateRating `json:"aggregateRating"`
+}
+
+type Address struct {
+	Country    string `json:"addressCountry"`
+	Region     string `json:"addressRegion"`
+	City       string `json:"addressLocality"`
+	Street     string `json:"streetAddress"`
+	PostalCode string `json:"postalCode"`
+}
+
+type AggregateRating struct {
+	RatingValue float64 `json:"ratingValue"`
+	BestRating  string  `json:"bestRating"`
+	WorstRating string  `json:"worstRating"`
+	ReviewCount int64   `json:"reviewCount"`
+}
