c4a320210685e9bfe17b1fd6c55c57dd4cd97203
hendri <hendri.chia@tiket.com>
2020-05-15 15:15:56 +0700

    FE : handling if user still have old cookie format, implement checking regex pattern  in PrivateRoute.js

--- ui/src/components/PrivateRoute.js
+++ ui/src/components/PrivateRoute.js
@@ -1,31 +1,42 @@
 import React from 'react';
+import { matchPath } from 'react-router';
 import { Route, Redirect, useLocation } from 'react-router-dom';
 import { useAuth } from './AuthProvider';
-import { isAllowed } from './AuthRole';
-
-// TODO : get value from cookie
-const roleUserLogin = {
-  rights: ['/datasources', '/mismatch-rule', '/rules', '/analytic', '/simulation', '/audit-trail', '/users'],
-};
 
 function PrivateRoute({ component: Component, ...rest }) {
   const auth = useAuth();
   const location = useLocation();
-  const firstPath = '/'.concat(location.pathname.split('/')[1]);
+  // const firstPath = '/'.concat(location.pathname.split('/')[1]);
 
-  let mn;
-  /*
-  // TODO : get value from cookie
-  Object.keys(auth.currentUser.modules).forEach((key) => {
-    mn = '/'.concat(auth.currentUser.modules[key]);
-  });
-  const roleUserLogin = {
-    rights: mn,
-  };
-  */
-  const isAllowAccess = isAllowed(roleUserLogin, [firstPath]);
+  let listModules = null;
+  let jsonModules = auth.currentUser === null ? null : auth.currentUser.modules;
 
-  // TODO : before return, check regex using function matchPath
+  let isMatch = false;
+  let loopStop = false;
+
+  if (jsonModules) {
+    jsonModules = JSON.parse(jsonModules);
+
+    Object.keys(jsonModules).forEach((key) => {
+      listModules = jsonModules[key];
+    });
+    if (location.pathname === '/') isMatch = true; // special case for Home URL (/)
+    else {
+      listModules.forEach((item, index) => {
+        if (loopStop) { return; }
+
+        // check regex pattern
+        isMatch = matchPath(location.pathname, {
+          path: '/'.concat(item.pattern),
+          exact: true,
+          strict: false,
+        });
+        if (isMatch) loopStop = true;
+      });
+    }
+    // if have not access Obj isMatch = null
+  }
+  const isAllowAccess = isMatch !== null;
 
   return (
     <Route
@@ -39,4 +50,3 @@ function PrivateRoute({ component: Component, ...rest }) {
 }
 
 export default PrivateRoute;
-
