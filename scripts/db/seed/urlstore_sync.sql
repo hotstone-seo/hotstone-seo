4685aafbe6223c29734c9662131baf34f3171ce1
iman tung <iman.tung@gmail.com>
2020-05-16 21:05:24 +0700

    add missing urlstore sync db seed

---
+++ scripts/db/seed/urlstore_sync.sql
@@ -0,0 +1,9 @@
+  
+INSERT INTO url_sync (version, "operation", rule_id, latest_url_pattern)
+VALUES
+    (1, 'INSERT', 1, '/airports'),
+    (2, 'INSERT', 2, '/hotels'),
+    (3, 'INSERT', 3, '/events'),
+    (4, 'INSERT', 4, '/events/<eventName>');
+
+SELECT setval('url_sync_version_seq', (SELECT MAX(version) FROM url_sync));
