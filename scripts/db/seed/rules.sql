0812f39fb20fa15ad2f3f7c1dca6891238a72844
Hawari Rahman <hawari.rahman17@gmail.com>
2020-05-20 14:47:51 +0700

    Modify broken seed scripts

--- scripts/db/seed/rules.sql
+++ scripts/db/seed/rules.sql
@@ -1,8 +1,12 @@
-INSERT INTO rules (id, data_source_id, "name", url_pattern)
+INSERT INTO rules (id, "name", url_pattern)
 VALUES 
-    (1, NULL, 'Airport Rule', '/airports'),
-    (2, NULL, 'Hotel Rule', '/hotels'),
-    (3, NULL, 'Event Rule', '/events'),
-    (4, 1, 'Event Rule with Path Params', '/events/<eventName>');
+    (1, 'Airport Rule', '/airports'),
+    (2, 'Hotel Rule', '/hotels'),
+    (3, 'Event Rule', '/events'),
+    (4, 'Event Rule with Path Params', '/events/<eventName>');
 
 SELECT setval('rules_id_seq', (SELECT MAX(id) FROM rules));
+
+INSERT INTO rule_data_sources (rule_id, data_source_id)
+VALUES
+    (4, 1);
