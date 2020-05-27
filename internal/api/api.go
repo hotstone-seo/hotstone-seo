1f9611f50398d6ec10cdb7d1a92415cfdcaef4f7
hendri <hendri.chia@tiket.com>
2020-05-27 18:08:36 +0700

    API : create middleware user access control

--- internal/api/api.go
+++ internal/api/api.go
@@ -34,6 +34,8 @@ func (a *API) SetRoute(e *echo.Echo) {
 	group := e.Group("/api")
 	group.Use(a.AuthCntrl.Middleware())
 	group.Use(a.AuthCntrl.SetTokenCtxMiddleware())
+	//TODO: middleware for check auth module. still on progress
+	//group.Use(a.AuthCntrl.CheckAuthModules())
 	group.Use(middleware.CORSWithConfig(middleware.DefaultCORSConfig))
 	group.Use(middleware.Recover())
 	group.POST("/logout", a.AuthCntrl.Logout)
