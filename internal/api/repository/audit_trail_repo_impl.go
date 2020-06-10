43ebc926e17efadc81bf504ca5b894fa1c72ae98
iman tung <iman.tung@gmail.com>
2020-06-10 17:09:28 +0700

    audittrail not support transaction

--- internal/api/repository/audit_trail_repo_impl.go
+++ internal/api/repository/audit_trail_repo_impl.go
@@ -19,13 +19,26 @@ type AuditTrailRepoImpl struct {
 func (r *AuditTrailRepoImpl) Find(ctx context.Context, paginationParam PaginationParam) (list []*AuditTrail, err error) {
 	var rows *sql.Rows
 	psql := sq.StatementBuilder.PlaceholderFormat(sq.Dollar)
-	builder := psql.Select("id", "time", "entity_name", "entity_id", "operation", "username", "old_data", "new_data").
-		From("audit_trail")
-	if rows, err = ComposePagination(builder, paginationParam).RunWith(dbtxn.DB(ctx, r)).QueryContext(ctx); err != nil {
+	builder := psql.
+		Select(
+			"id",
+			"time",
+			"entity_name",
+			"entity_id",
+			"operation",
+			"username",
+			"old_data",
+			"new_data",
+		).
+		From("audit_trail").
+		RunWith(r)
+
+	if rows, err = ComposePagination(builder, paginationParam).QueryContext(ctx); err != nil {
 		dbtxn.SetError(ctx, err)
 		return
 	}
 	defer rows.Close()
+
 	list = make([]*AuditTrail, 0)
 	for rows.Next() {
 		var rule *AuditTrail
@@ -41,10 +54,17 @@ func (r *AuditTrailRepoImpl) Find(ctx context.Context, paginationParam Paginatio
 // Insert auditTrail
 func (r *AuditTrailRepoImpl) Insert(ctx context.Context, m AuditTrail) (lastInsertID int64, err error) {
 	query := sq.Insert("audit_trail").
-		Columns("entity_name", "entity_id", "operation", "username", "old_data", "new_data").
+		Columns(
+			"entity_name",
+			"entity_id",
+			"operation",
+			"username",
+			"old_data",
+			"new_data",
+		).
 		Values(m.EntityName, m.EntityID, m.Operation, m.Username, m.OldData, m.NewData).
 		Suffix("RETURNING \"id\"").
-		RunWith(dbtxn.DB(ctx, r)).
+		RunWith(r).
 		PlaceholderFormat(sq.Dollar)
 	if err = query.QueryRowContext(ctx).Scan(&m.ID); err != nil {
 		dbtxn.SetError(ctx, err)
@@ -56,8 +76,16 @@ func (r *AuditTrailRepoImpl) Insert(ctx context.Context, m AuditTrail) (lastInse
 
 func scanAuditTrail(rows *sql.Rows) (*AuditTrail, error) {
 	var m AuditTrail
-	var err error
-	if err = rows.Scan(&m.ID, &m.Time, &m.EntityName, &m.EntityID, &m.Operation, &m.Username, &m.OldData, &m.NewData); err != nil {
+	if err := rows.Scan(
+		&m.ID,
+		&m.Time,
+		&m.EntityName,
+		&m.EntityID,
+		&m.Operation,
+		&m.Username,
+		&m.OldData,
+		&m.NewData,
+	); err != nil {
 		return nil, err
 	}
 	return &m, nil
