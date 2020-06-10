5618ec9b1dde6cdc15185361d020478b810461f1
Fahri Nurul Hidayat <fahri.cyber@gmail.com>
2020-06-10 09:21:36 +0700

    HotStoneClient: remove client side handling; tidy up

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,7 @@
 import React, { useState } from "react";
 import { Helmet } from "react-helmet";
-import { withRouter } from "react-router-dom";
 import queryString from "query-string"
 import cachingFetch from "make-fetch-happen"
-
-
-/**
- * Returns a tag object after its string values have been interpolated with data
- *
- * @param {Object} tag - Tag object which contains values which may be a string template
- * @param {Object} data - The mapping for interpolating tag's string placeholders
- *
- * TODO: Provide implementation by iterating through all of tag's value
- */
-function interpolate(tag, data) {
-  // NOTE: This implementation is O(n2), please provide better implementation in the future
-  for (let [key, value] of Object.entries(tag)) {
-    tag[key] = value.replace(/{(\w+)}/g, function(match, capture) {
-      return data[capture] || "";
-    });
-  }
-
-  return tag;
-}
 
 class HotStoneClient {
   constructor(hostURL, key, opts = {}) {
@@ -82,59 +61,31 @@ class HotStoneClient {
 
 const HotStoneContext = React.createContext([]);
 
-// NOTE: What this comppnent should do:
-// Manage meta tag which responds to path changes.
-//
-// There's a hook provided by react-router, useLocation, might be a good starting point
-// but learn about React Hooks first!
-//
-// TODO: Find a way to detect path change
 class HotStoneWrapper extends React.Component {
   constructor(props) {
-    super(props);
+      super(props);
 
-    const { tags, client } = props;
-    this.state = { tags };
-    this.client = client;
-    this.fetchTags = this.fetchTags.bind(this);
-  }
-
-  shouldComponentUpdate(nextProps, nextState) {
-    if (this.props.location.pathname === nextProps.location.pathname) {
-      return false;
-    }
-    return true;
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
-      this.fetchTags(this.props.location.pathname);
-    }
-  }
-
-  async fetchTags(path) {
-    const rule = await this.client.match(path);
-    const tags = await this.client.tags(rule);
-    this.setState({ tags });
+      const { tags } = props;
+      this.state = { tags };
   }
 
   render() {
-    const { tags } = this.state;
-    const tagElements = tags.map(({ id, type, attributes, value }) => {
-      attributes.key = id;
-      return React.createElement(type, attributes, value);
-    });
-    return (
-      <div>
-        <Helmet>{tagElements}</Helmet>
-        <HotStoneContext.Provider value={tags}>
-          {this.props.children}
-        </HotStoneContext.Provider>
-      </div>
-    );
+      const { tags } = this.state;
+      const tagElements = tags.map(({ id, type, attributes, value }) => {
+          attributes.key = id;
+          return React.createElement(type, attributes, value);
+      });
+      return (
+          <div>
+              <Helmet>{tagElements}</Helmet>
+              <HotStoneContext.Provider value={tags}>
+                  {this.props.children}
+              </HotStoneContext.Provider>
+          </div>
+      );
   }
 }
 
-const HotStone = withRouter(HotStoneWrapper);
+const HotStone = HotStoneWrapper;
 
 export { HotStone, HotStoneClient, HotStoneContext };
