b203392dd826f77954833332f60038917fff6a52
Hawari Rahman <hawari.rahman17@gmail.com>
2020-06-17 13:23:32 +0700

    Merge current work with master

--- internal/api/service/tag_service.go
+++ internal/api/service/tag_service.go
@@ -8,27 +8,33 @@ import (
 	"github.com/hotstone-seo/hotstone-seo/pkg/dbtxn"
 	log "github.com/sirupsen/logrus"
 	"github.com/typical-go/typical-rest-server/pkg/dbkit"
+	"github.com/typical-go/typical-rest-server/pkg/errvalid"
 	"go.uber.org/dig"
 )
 
-// TagService provides available method to be used for managing Tag entity
-// @mock
-type TagService interface {
-	FindByRuleAndLocale(ctx context.Context, ruleID int64, locale string) ([]*repository.Tag, error)
-	repository.TagRepo
-}
+type (
+	// TagService provides available method to be used for managing Tag entity
+	// @mock
+	TagService interface {
+		Find(context.Context, map[string][]string) ([]*repository.Tag, error)
+		FindOne(context.Context, string) (*repository.Tag, error)
+		Create(context.Context, repository.Tag) (id string, err error)
+		Update(context.Context, string, repository.Tag) error
+		Delete(context.Context, string) error
+	}
 
-// TagServiceImpl is the implementation of TagService
-type TagServiceImpl struct {
-	dig.In
-	repository.TagRepo
-	dbtxn.Transactional
-	AuditTrailService AuditTrailService
-	HistoryService    HistoryService
-}
+	// TagServiceImpl is the implementation of TagService
+	TagServiceImpl struct {
+		dig.In
+		repository.TagRepo
+		dbtxn.Transactional
+		AuditTrailService AuditTrailService
+		HistoryService    HistoryService
+	}
+)
 
-// NewTagService return new instance of TagService
-// @ctor
+// NewTagService returns new instance of TagService
+// @constructor
 func NewTagService(impl TagServiceImpl) TagService {
 	return &impl
 }
@@ -41,8 +47,36 @@ func (s *TagServiceImpl) FindByRuleAndLocale(ctx context.Context, ruleID int64, 
 	)
 }
 
-// Insert creates a new Tag entity
-func (s *TagServiceImpl) Insert(ctx context.Context, tag repository.Tag) (newID int64, err error) {
+// Find returns list of Tag entity based on provided filters
+//
+// TODO: Since its possible for QueryParams to provide array of strings, we should extend
+// dbkit to support "IN" query
+func (s *TagServiceImpl) Find(ctx context.Context, filters map[string][]string) ([]*repository.Tag, error) {
+	selectOpts := make([]dbkit.SelectOption, 0)
+	if ruleFilter, exists := filters["rule_id"]; exists && len(ruleFilter) > 0 {
+		ruleID, err := strconv.ParseInt(ruleFilter[0], 10, 64)
+		if err != nil {
+			return nil, errvalid.New("rule_id is not valid")
+		}
+		selectOpts = append(selectOpts, dbkit.Equal("rule_id", ruleID))
+	}
+	if localeFilter, exists := filters["locale"]; exists && len(localeFilter) > 0 {
+		selectOpts = append(selectOpts, dbkit.Equal("locale", localeFilter[0]))
+	}
+	return s.TagRepo.Find(ctx, selectOpts...)
+}
+
+// FindOne returns a single Tag entity with provided ID
+func (s *TagServiceImpl) FindOne(ctx context.Context, id string) (*repository.Tag, error) {
+	tagID, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		return nil, errvalid.New("ID is not valid")
+	}
+	return s.TagRepo.FindOne(ctx, tagID)
+}
+
+// Create creates a new Tag entity
+func (s *TagServiceImpl) Create(ctx context.Context, tag repository.Tag) (id int64, err error) {
 	defer func() {
 		if err != nil {
 			return
@@ -51,7 +85,7 @@ func (s *TagServiceImpl) Insert(ctx context.Context, tag repository.Tag) (newID 
 			ctx,
 			Record{
 				EntityName: "tags",
-				EntityID:   newID,
+				EntityID:   id,
 				Operation:  InsertOp,
 				PrevData:   nil,
 				NextData:   tag,
@@ -62,6 +96,9 @@ func (s *TagServiceImpl) Insert(ctx context.Context, tag repository.Tag) (newID 
 	}()
 	if tag.Attributes == nil {
 		tag.Attributes = map[string]string{}
+	}
+	if err = tag.Validate(); err != nil {
+		return
 	}
 	return s.TagRepo.Insert(ctx, tag)
 }
